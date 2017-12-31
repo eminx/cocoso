cd3ac8e6c124c3a3295a832554ee3796638fc4d6
Emin Durak <emin@pomegra.org>
2017-12-31 18:02:56 +0100

    added the accounts system

--- client/Nodal.js
+++ client/Nodal.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import Blaze from 'meteor/gadicc:blaze-react-component';
 
 class Nodal extends React.Component { 
   render(){
 	  return (
 	  	<div>
-       <p>Your subscription ID is: {this.props.subID} </p>
-       <button onClick={this.props.incrementSubId}>Click Me To Increment</button>
+	  		<Blaze template="loginButtons" />
+       	<p>Your subscription ID is: {this.props.subID} </p>
+       	<button onClick={this.props.incrementSubId}>Click Me To Increment</button>
      </div>
     )
 	}
