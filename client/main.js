dd39aed6aa68719d76713fc05e6fe9bccbd4ec18
Emin Durak <emin@pomegra.org>
2018-01-11 19:38:48 +0100

    removed the semantic

--- client/main.js
+++ client/main.js
@@ -3,7 +3,6 @@ import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
 import { renderRoutes } from './RouterComponents/routes';
 import 'antd/dist/antd.min.css';
-import 'semantic-ui-css/semantic.min.css';
 
 Meteor.startup(() => {
   render(renderRoutes() , document.getElementById('render-target'));
