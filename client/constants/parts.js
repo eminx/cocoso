2aa1f499c2794f2a4b30023f44911871bb22b359
Emin Durak <emin@pomegra.org>
2018-01-13 15:10:31 +0100

    added noden parts

---
+++ client/constants/parts.js
@@ -0,0 +1,13 @@
+// value is lowercase, unique and one letter. Use under_score instead of camelCase if you need to
+
+const nodenParts = [
+	{value: 'library', name: 'The Library'},
+	{value: 'cinema', name: 'The Cinema'},
+	{value: 'techno_temple', name: 'The Techno Temple'},
+	{value: 'living_room', name: 'The Living Room'},
+	{value: 'lounge', name: 'The Lounge'},
+	{value: 'coffee_space', name: 'The Coffee Space'},
+	{value: 'meditation_room', name: 'The Meditation Room'}
+];
+
+export default nodenParts;
