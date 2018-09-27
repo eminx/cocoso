17c3ee08fcb9b32622118ffee5dece018b19d50f
Emin Durak <emin@pomegra.org>
2018-09-28 01:43:36 +0200

    fixed the router bug

--- client/RouterComponents/routes.js
+++ client/RouterComponents/routes.js
@@ -22,29 +22,31 @@ const browserHistory = createBrowserHistory();
 
 export const renderRoutes = () => (
   <Router history={browserHistory}>
-    <LayoutContainer history={browserHistory}>
-      <Switch>
-        <Route exact path="/" component={HomeContainer} />
+    <Switch>
+      <LayoutContainer history={browserHistory}>
+        <Switch>
+          <Route exact path="/" component={HomeContainer} />
 
-        <Route exact path="/new-booking" component={NewBookSpaceContainer} />
-        <Route path="/booking/:id" component={BookingContainer} />
-        <Route path="/edit-booking/:id/" component={EditBookingContainer} />
+          <Route exact path="/new-booking" component={NewBookSpaceContainer} />
+          <Route path="/booking/:id" component={BookingContainer} />
+          <Route path="/edit-booking/:id/" component={EditBookingContainer} />
 
-        <Route exact path="/new-group" component={NewGroupContainer} />
-        <Route path="/groups/" component={GroupsListContainer} />
-        <Route path="/group/:id" component={GroupContainer} />
-        <Route path="/edit-group/:id/" component={EditGroupContainer} />
+          <Route exact path="/new-group" component={NewGroupContainer} />
+          <Route path="/groups/" component={GroupsListContainer} />
+          <Route path="/group/:id" component={GroupContainer} />
+          <Route path="/edit-group/:id/" component={EditGroupContainer} />
 
-        <Route
-          path="/my-profile/"
-          history={browserHistory}
-          component={ProfileContainer}
-        />
+          <Route
+            path="/my-profile/"
+            history={browserHistory}
+            component={ProfileContainer}
+          />
 
-        <Route path="/users" component={UsersContainer} />
+          <Route path="/users" component={UsersContainer} />
 
-        {/*<Route path="*" component={NotFoundPage}/>*/}
-      </Switch>
-    </LayoutContainer>
+          {/*<Route path="*" component={NotFoundPage}/>*/}
+        </Switch>
+      </LayoutContainer>
+    </Switch>
   </Router>
 );
