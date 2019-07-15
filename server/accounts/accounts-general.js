c4390933995aa4231a4c2f6cc8f854ad5e2c1b1a
Emin Durak <emin@tuta.io>
2019-07-16 02:39:33 +0300

    removed fb account code

--- server/accounts/accounts-general.js
+++ server/accounts/accounts-general.js
@@ -9,13 +9,13 @@ Accounts.onCreateUser((options, user) => {
   user.attending = [];
   user.groups = [];
   user.notifications = [];
-  if (user.services.facebook) {
-    user.username = user.services.facebook.name;
-    user.emails = [{ address: user.services.facebook.email }];
-  } else if (user.services.google) {
-    user.username = user.services.google.name;
-    user.emails = [{ address: user.services.google.email }];
-  }
+  // if (user.services.facebook) {
+  //   user.username = user.services.facebook.name;
+  //   user.emails = [{ address: user.services.facebook.email }];
+  // } else if (user.services.google) {
+  //   user.username = user.services.google.name;
+  //   user.emails = [{ address: user.services.google.email }];
+  // }
   Meteor.call(
     'sendEmail',
     user.emails[0].address,
