abdca9d6bd7d94b20607a22cfad00ce4f55b4519
Emin Durak <emin@tuta.io>
2019-10-28 21:12:37 +0300

    now sending email to non members as well

--- server/methods/email-methods.js
+++ server/methods/email-methods.js
@@ -1,28 +1,28 @@
-import { Email } from 'meteor/email';
+import { Email } from 'meteor/email'
 
-function isValidEmail(email) {
-  var re = /\S+@\S+\.\S+/;
-  return re.test(email);
+function isValidEmail (email) {
+  var re = /\S+@\S+\.\S+/
+  return re.test(email)
 }
 
 Meteor.methods({
   sendEmail: (id, subjectEmail, textEmail, isNewUser) => {
-    if (!isNewUser && !Meteor.userId()) {
-      return false;
-    }
+    // if (!isNewUser && !Meteor.userId()) {
+    //   return false;
+    // }
 
-    check([id, subjectEmail, textEmail], [String]);
+    check([id, subjectEmail, textEmail], [String])
     // Let other method calls from the same client start running,
     // without waiting for the email sending to complete.
-    var fromEmail = Meteor.settings.mailCredentials.smtp.fromEmail;
-    var toEmail;
+    var fromEmail = Meteor.settings.mailCredentials.smtp.fromEmail
+    var toEmail
 
     if (isValidEmail(id)) {
-      toEmail = id;
+      toEmail = id
     } else if (id.length === 17) {
-      toEmail = Meteor.users.findOne({ _id: id }).emails[0].address;
+      toEmail = Meteor.users.findOne({ _id: id }).emails[0].address
     } else {
-      toEmail = Meteor.users.findOne({ username: id }).emails[0].address;
+      toEmail = Meteor.users.findOne({ username: id }).emails[0].address
     }
 
     Email.send({
@@ -30,6 +30,6 @@ Meteor.methods({
       to: toEmail,
       subject: subjectEmail,
       text: textEmail
-    });
+    })
   }
-});
+})
