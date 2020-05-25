9cf4e61c81d466d2e18d475affcf20ee89f126b5
Emin Durak <emin@tuta.io>
2020-05-25 21:03:40 +0530

    now it's functional

--- client/RouterComponents/works/Work.jsx
+++ client/RouterComponents/works/Work.jsx
@@ -1,4 +1,4 @@
-import React, { PureComponent } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import { Box, Button, Avatar, Heading, Text } from 'grommet';
 
 import { UserContext } from '../../LayoutContainer';
@@ -7,111 +7,94 @@ import Template from '../../UIComponents/Template';
 import NiceSlider from '../../UIComponents/NiceSlider';
 import { call } from '../../functions';
 
-class Work extends PureComponent {
-  state = {
-    work: null,
-    isLoading: true
-  };
+const Work = ({ history, match }) => {
+  const [work, setWork] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const { currentUser } = useContext(UserContext);
 
-  componentDidMount() {
-    this.getWork();
-  }
+  useEffect(() => {
+    getWork();
+  }, []);
 
-  getWork = async () => {
-    this.setState({ isLoading: true });
-    const { match } = this.props;
-
+  const getWork = async () => {
     const workId = match.params.workId;
     const username = match.params.username;
 
-    // try {
-    const response = await call('getWork', workId, username);
-    this.setState({
-      work: response,
-      isLoading: false
-    });
-    // } catch (error) {
-    //   message.error(error.reason);
-    //   this.setState({
-    //     isLoading: false
-    //   });
-    // }
+    try {
+      const response = await call('getWork', workId, username);
+      setWork(response);
+      setLoading(false);
+    } catch (error) {
+      message.error(error.reason);
+      setLoading(false);
+    }
   };
 
-  render() {
-    const { currentUser } = this.context;
-    const { history, match } = this.props;
-    const { work, isLoading } = this.state;
-
-    if (!work || isLoading) {
-      return <Loader />;
-    }
-
-    const author =
-      work.authorFirstName && work.authorLastName
-        ? work.authorFirstName + ' ' + work.authorLastName
-        : work.authorUsername;
-
-    const isOwner =
-      currentUser && currentUser.username === match.params.username;
-
-    return (
-      <Template
-        leftContent={
-          <Box pad={{ bottom: 'medium' }}>
-            <Heading pad="small" level={2}>
-              {work.title}
-            </Heading>
-            <Text>{work.shortDescription}</Text>
-          </Box>
-        }
-        rightContent={
-          <Box>
-            <Box
-              round
-              background="light-2"
-              direction="row"
-              justify="between"
-              align="center"
-            >
-              <Text margin={{ left: 'medium' }} weight="bold">
-                {work.authorUsername}
-              </Text>
-              <Avatar src="//s.gravatar.com/avatar/b7fb138d53ba0f573212ccce38a7c43b?s=80" />
-            </Box>
-            <Box
-              pad={{
-                top: 'medium',
-                bottom: 'small'
-              }}
-            >
-              <Text>{work.additionalInfo}</Text>
-            </Box>
-          </Box>
-        }
-      >
-        <NiceSlider images={work.images} />
-        <Box pad={{ top: 'medium' }}>
-          <div dangerouslySetInnerHTML={{ __html: work.longDescription }} />
-        </Box>
-        <Box margin={{ top: 'large', bottom: 'large' }} justify="end">
-          {isOwner && (
-            <Button
-              size="small"
-              onClick={() =>
-                history.push(
-                  `/${currentUser.username}/edit-work/${match.params.workId}`
-                )
-              }
-              label="Edit this work"
-            />
-          )}
-        </Box>
-      </Template>
-    );
+  if (!work || loading) {
+    return <Loader />;
   }
-}
 
-Work.contextType = UserContext;
+  const author =
+    work.authorFirstName && work.authorLastName
+      ? work.authorFirstName + ' ' + work.authorLastName
+      : work.authorUsername;
+
+  const isOwner = currentUser && currentUser.username === match.params.username;
+
+  return (
+    <Template
+      leftContent={
+        <Box pad={{ bottom: 'medium' }}>
+          <Heading pad="small" level={2}>
+            {work.title}
+          </Heading>
+          <Text>{work.shortDescription}</Text>
+        </Box>
+      }
+      rightContent={
+        <Box>
+          <Box
+            round
+            background="light-2"
+            direction="row"
+            justify="between"
+            align="center"
+          >
+            <Text margin={{ left: 'medium' }} weight="bold">
+              {work.authorUsername}
+            </Text>
+            <Avatar src="//s.gravatar.com/avatar/b7fb138d53ba0f573212ccce38a7c43b?s=80" />
+          </Box>
+          <Box
+            pad={{
+              top: 'medium',
+              bottom: 'small'
+            }}
+          >
+            <Text>{work.additionalInfo}</Text>
+          </Box>
+        </Box>
+      }
+    >
+      <NiceSlider images={work.images} />
+      <Box pad={{ top: 'medium' }}>
+        <div dangerouslySetInnerHTML={{ __html: work.longDescription }} />
+      </Box>
+      <Box margin={{ top: 'large', bottom: 'large' }} justify="end">
+        {isOwner && (
+          <Button
+            size="small"
+            onClick={() =>
+              history.push(
+                `/${currentUser.username}/edit-work/${match.params.workId}`
+              )
+            }
+            label="Edit this work"
+          />
+        )}
+      </Box>
+    </Template>
+  );
+};
 
 export default Work;
