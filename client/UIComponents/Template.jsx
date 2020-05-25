d0efbcd19144bdfd7866fefbc48316be73352eed
Emin Durak <emin@tuta.io>
2020-05-26 00:55:08 +0530

    made the group less wide

--- client/UIComponents/Template.jsx
+++ client/UIComponents/Template.jsx
@@ -2,13 +2,20 @@ import React from 'react';
 import { Container, Row, Col } from 'react-grid-system';
 import { Box, Heading } from 'grommet';
 
+const colStyle = {
+  maxWidth: 600,
+  margin: '0 auto',
+};
+
 function Template({ leftContent, rightContent, heading, children }) {
   return (
     <Container fluid style={{ width: '100%' }}>
       <Row gutterWidth={12}>
-        <Col lg={3}>{leftContent}</Col>
-        <Col lg={6}>
-          <Box style={{ maxWidth: 600, margin: '0 auto' }}>
+        <Col lg={3} style={colStyle}>
+          {leftContent}
+        </Col>
+        <Col lg={6} style={colStyle}>
+          <Box>
             {heading && (
               <Box pad={{ top: 'small', bottom: 'small' }}>
                 <Heading level={3}>{heading}</Heading>
@@ -17,7 +24,9 @@ function Template({ leftContent, rightContent, heading, children }) {
             {children}
           </Box>
         </Col>
-        <Col lg={3}>{rightContent}</Col>
+        <Col lg={3} style={colStyle}>
+          {rightContent}
+        </Col>
       </Row>
     </Container>
   );
