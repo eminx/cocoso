32c748f1b65acbf77a44fea205ea004d239af9ba
Emin Durak <emin@tuta.io>
2020-05-25 23:38:26 +0530

    removed antd antd completely

--- client/main.js
+++ client/main.js
@@ -6,12 +6,11 @@ import { render } from 'react-dom';
 import App from './App';
 
 import 'react-big-calendar/lib/css/react-big-calendar.css';
-import 'antd/dist/antd.min.css';
 import 'react-quill/dist/quill.snow.css';
 
 Meteor.startup(() => {
   Accounts.ui.config({
-    passwordSignupFields: 'USERNAME_AND_EMAIL'
+    passwordSignupFields: 'USERNAME_AND_EMAIL',
   });
   render(<App />, document.getElementById('render-target'));
 });
