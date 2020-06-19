a01bb5ef06685fa2085915c9e87a6901d728eb06
Emin Durak <emin@tuta.io>
2020-06-19 15:37:04 +0530

    started customisations

--- client/RouterComponents/admin/Settings.jsx
+++ client/RouterComponents/admin/Settings.jsx
@@ -1,6 +1,16 @@
 import { Meteor } from 'meteor/meteor';
 import React, { PureComponent } from 'react';
-import { Anchor, TextInput, FormField, Form, Box, Text, Button } from 'grommet';
+import {
+  Anchor,
+  Heading,
+  TextInput,
+  FormField,
+  Form,
+  Box,
+  Text,
+  Button,
+} from 'grommet';
+const pluralize = require('pluralize');
 
 import { UserContext } from '../../LayoutContainer';
 import Loader from '../../UIComponents/Loader';
@@ -10,14 +20,14 @@ import message from '../../UIComponents/message';
 
 const menuRoutes = [
   { label: 'Settings', value: '/admin/settings' },
-  { label: 'Members', value: '/admin/members' }
+  { label: 'Members', value: '/admin/members' },
 ];
 
 class Settings extends PureComponent {
   state = {
     settings: null,
     isLoading: true,
-    isFormValueChanged: false
+    isFormValueChanged: false,
   };
 
   componentDidMount() {
@@ -28,14 +38,14 @@ class Settings extends PureComponent {
     const { settings } = this.context;
     this.setState({
       settings,
-      isLoading: false
+      isLoading: false,
     });
   };
 
-  handleFormChange = settings => {
+  handleFormChange = (settings) => {
     this.setState({
       settings: settings,
-      isFormValueChanged: true
+      isFormValueChanged: true,
     });
   };
 
@@ -59,14 +69,14 @@ class Settings extends PureComponent {
         message.error(error.reason);
         this.setState({
           isLoading: false,
-          isError: true
+          isError: true,
         });
         return;
       }
       message.success('Settings are successfully saved');
       this.setState({
         isLoading: false,
-        isSuccess: true
+        isSuccess: true,
       });
     });
   };
@@ -88,10 +98,10 @@ class Settings extends PureComponent {
 
     return (
       <Template
-        heading="Organisation Settings"
+        heading="Settings"
         leftContent={
           <ListMenu list={menuRoutes}>
-            {datum => (
+            {(datum) => (
               <Anchor
                 onClick={() => history.push(datum.value)}
                 key={datum.value}
@@ -105,52 +115,149 @@ class Settings extends PureComponent {
           </ListMenu>
         }
       >
-        <Form
-          value={settings}
-          onChange={this.handleFormChange}
-          onSubmit={this.handleFormSubmit}
-        >
-          <FormField label="Name">
-            <TextInput
-              plain={false}
-              name="name"
-              placeholder="Sandy Art Space"
-            />
-          </FormField>
+        <Box>
+          <Heading level={3}>Organisation</Heading>
+          <Form
+            value={settings}
+            onChange={this.handleFormChange}
+            onSubmit={this.handleFormSubmit}
+          >
+            <FormField label="Name">
+              <TextInput
+                plain={false}
+                name="name"
+                placeholder="Sandy Art Space"
+              />
+            </FormField>
 
-          <FormField label="Email address">
-            <TextInput
-              plain={false}
-              name="email"
-              placeholder="contact@sandyartspace.net"
-            />
-          </FormField>
+            <FormField label="Email address">
+              <TextInput
+                plain={false}
+                name="email"
+                placeholder="contact@sandyartspace.net"
+              />
+            </FormField>
 
-          <FormField label="Address">
-            <TextInput
-              plain={false}
-              name="address"
-              placeholder="Karl Marx strasse 99"
-            />
-          </FormField>
+            <FormField label="Address">
+              <TextInput
+                plain={false}
+                name="address"
+                placeholder="Karl Marx strasse 99"
+              />
+            </FormField>
 
-          <FormField label="City">
-            <TextInput plain={false} name="city" placeholder="Berlin" />
-          </FormField>
+            <FormField label="City">
+              <TextInput plain={false} name="city" placeholder="Berlin" />
+            </FormField>
 
-          <FormField label="Country">
-            <TextInput plain={false} name="country" placeholder="Sri Lanka" />
-          </FormField>
+            <FormField label="Country">
+              <TextInput plain={false} name="country" placeholder="Sri Lanka" />
+            </FormField>
 
-          <Box direction="row" justify="end" pad="small">
-            <Button
-              type="submit"
-              primary
-              label="Confirm"
-              disabled={!isFormValueChanged}
-            />
-          </Box>
-        </Form>
+            <Box direction="row" justify="end" pad="small">
+              <Button
+                type="submit"
+                primary
+                label="Confirm"
+                disabled={!isFormValueChanged}
+              />
+            </Box>
+          </Form>
+        </Box>
+
+        <Box>
+          <Heading level={3}>Wording</Heading>
+          <Form
+            value={settings}
+            onChange={this.handleFormChange}
+            onSubmit={this.handleFormSubmit}
+          >
+            <Box width="medium" margin={{ bottom: 'medium' }}>
+              <FormField
+                label="Process"
+                size="small"
+                help={
+                  <Text size="small">
+                    Type a name if you want to replace{' '}
+                    <b>
+                      <code>process</code>
+                    </b>{' '}
+                    with something else. Note that only one word is allowed
+                  </Text>
+                }
+              >
+                <TextInput plain={false} name="process" placeholder="group" />
+              </FormField>
+
+              <Text size="small" margin={{ left: 'small' }}>
+                {settings.process && (
+                  <span>
+                    <b>
+                      <code>{pluralize(settings.process)}</code>
+                    </b>{' '}
+                    will be plural version
+                  </span>
+                )}
+              </Text>
+            </Box>
+
+            <Box width="medium" margin={{ bottom: 'medium' }}>
+              <FormField
+                label="Work"
+                size="small"
+                help={
+                  <Text size="small">
+                    Type a name if you want to replace{' '}
+                    <b>
+                      <code>work</code>
+                    </b>{' '}
+                    with something else. Note that only one word is allowed
+                  </Text>
+                }
+              >
+                <TextInput plain={false} name="work" placeholder="offer" />
+              </FormField>
+            </Box>
+
+            <Box width="medium" margin={{ bottom: 'medium' }}>
+              <FormField
+                label="Info"
+                size="small"
+                help={
+                  <Text size="small">
+                    Type a name if you want to replace{' '}
+                    <b>
+                      <code>Info</code>
+                    </b>{' '}
+                    with something else. Note that only one word is allowed
+                  </Text>
+                }
+              >
+                <TextInput plain={false} name="info" placeholder="About" />
+              </FormField>
+
+              <Text size="small" margin={{ left: 'small' }}>
+                {settings.info && (
+                  <span>
+                    <b>
+                      <code>{pluralize(settings.info)}</code>
+                    </b>{' '}
+                    will be plural version
+                  </span>
+                )}
+              </Text>
+            </Box>
+
+            <Box direction="row" justify="end" pad="small">
+              <Button
+                type="submit"
+                primary
+                label="Confirm"
+                disabled={!isFormValueChanged}
+              />
+            </Box>
+          </Form>
+        </Box>
       </Template>
     );
   }
