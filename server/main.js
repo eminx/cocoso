0fdf32108c281f85f8414002fc993b61db7efc84
Emin <mino@Emins-MacBook-Pro.local>
2020-08-14 15:52:38 +0530

    on launch update to the new setting

--- server/main.js
+++ server/main.js
@@ -3,6 +3,32 @@ import { Meteor } from 'meteor/meteor';
 Meteor.startup(() => {
   const smtp = Meteor.settings.mailCredentials.smtp;
 
+  Meteor.users.find().forEach((user) => {
+    Meteor.users.update(user._id, {
+      $set: {
+        memberships: [
+          {
+            host: 'cic.nodal.app',
+            hostId: '3Er9qxkYQGB9obsEF',
+            role: 'contributor',
+            date: new Date(),
+          },
+        ],
+      },
+    });
+
+    Hosts.update('3Er9qxkYQGB9obsEF', {
+      $set: {
+        members: {
+          id: user._id,
+          email: user.emails[0].address,
+          role: 'contributor',
+          date: new Date(),
+        },
+      },
+    });
+  });
+
   process.env.MAIL_URL =
     'smtps://' +
     encodeURIComponent(smtp.userName) +
