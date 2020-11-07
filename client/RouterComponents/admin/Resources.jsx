a70efc4d8d4c97ef461f527e83f709faed26d289
Emin Durak <emin.tuta.io>
2020-11-07 03:36:11 +0300

    implemented CRUD for resources

---
+++ client/RouterComponents/admin/Resources.jsx
@@ -0,0 +1,245 @@
+import React, { useState, useEffect, useContext } from 'react';
+import {
+  Anchor,
+  Box,
+  Button,
+  Form,
+  FormField,
+  Heading,
+  Layer,
+  Text,
+  TextArea,
+  TextInput,
+} from 'grommet';
+import { FormAdd } from 'grommet-icons';
+import moment from 'moment';
+
+import NiceList from '../../UIComponents/NiceList';
+import Loader from '../../UIComponents/Loader';
+import Template from '../../UIComponents/Template';
+import ListMenu from '../../UIComponents/ListMenu';
+import { message, Alert } from '../../UIComponents/message';
+import { call } from '../../functions';
+import { StateContext } from '../../LayoutContainer';
+import { adminMenu } from '../../constants/general';
+
+function Resources({ history }) {
+  const [loading, setLoading] = useState(true);
+  const [resources, setResources] = useState([]);
+  const [showModal, setShowModal] = useState(false);
+  const [modalContent, setModalContent] = useState(null);
+  // const [sortBy, setSortBy] = useState('join-date');
+  // const [filter, setFilter] = useState('all');
+  // const [filterWord, setFilterWord] = useState('');
+  const { currentUser, currentHost, canCreateContent, role } = useContext(
+    StateContext
+  );
+
+  const getAndSetResources = async () => {
+    setLoading(true);
+    try {
+      const resources = await call('getResources');
+      setResources(resources);
+      setLoading(false);
+    } catch (error) {
+      message.error(error.error || error.reason);
+      console.log(error);
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getAndSetResources();
+  }, []);
+
+  if (loading) {
+    return <Loader />;
+  }
+
+  const handleSubmit = async () => {
+    if (modalContent.label.length < 3) {
+      message.error('Resource name is too short. Minimum 3 letters required');
+      return;
+    }
+    if (resources.some((resource) => resource.label === modalContent.label)) {
+      message.error('There already is a resource with this name');
+      return;
+    }
+    try {
+      if (modalContent.edit) {
+        const value = {
+          label: modalContent.label,
+          description: modalContent.description,
+        };
+        await call('updateResource', modalContent.id, value);
+        message.success('Resource successfully updated');
+      } else {
+        await call('createResource', modalContent);
+        message.success('Resource successfully added');
+      }
+      setModalContent(null);
+      setShowModal(false);
+      getAndSetResources();
+    } catch (error) {
+      console.log(error);
+      message.error(error.reason || error.error);
+    }
+  };
+
+  const initiateEditDialog = (resource) => {
+    setShowModal(true);
+    setModalContent({
+      label: resource.label,
+      description: resource.description,
+      id: resource._id,
+      edit: true,
+    });
+  };
+
+  const deleteResource = async (resourceId) => {
+    try {
+      await call('deleteResource', resourceId);
+      getAndSetResources();
+      message.success('Resource successfully deleted');
+    } catch (error) {
+      message.error(error.error || error.reason);
+      console.log(error);
+    }
+  };
+
+  const pathname = history && history.location.pathname;
+
+  const resourcesWithActions = resources.map((resource) => ({
+    ...resource,
+    actions: [
+      {
+        content: 'Edit',
+        handleClick: () => initiateEditDialog(resource),
+        isDisabled:
+          role !== 'admin' && resource.authorUsername !== currentUser.username,
+      },
+      {
+        content: 'Delete',
+        handleClick: () => deleteResource(resource._id),
+        isDisabled:
+          role !== 'admin' && resource.authorUsername !== currentUser.username,
+      },
+    ],
+  }));
+
+  return (
+    <Template
+      heading="Resources"
+      leftContent={
+        <Box pad="medium">
+          <ListMenu list={adminMenu}>
+            {(datum) => (
+              <Anchor
+                onClick={() => history.push(datum.value)}
+                key={datum.value}
+                label={
+                  <Text weight={pathname === datum.value ? 'bold' : 'normal'}>
+                    {datum.label}
+                  </Text>
+                }
+              />
+            )}
+          </ListMenu>
+        </Box>
+      }
+    >
+      {canCreateContent && (
+        <Box
+          direction="row"
+          justify="center"
+          width="100%"
+          margin={{ bottom: 'medium' }}
+        >
+          <Button
+            size="small"
+            label="NEW"
+            primary
+            icon={<FormAdd />}
+            onClick={() => {
+              setShowModal(true);
+              setModalContent(null);
+            }}
+          />
+        </Box>
+      )}
+
+      <Box pad="medium" background="white" margin={{ bottom: 'large' }}>
+        <NiceList list={resourcesWithActions} border="horizontal" pad="small">
+          {(resource) => (
+            <div key={resource.label}>
+              <Text size="large" weight="bold" margin={{ bottom: 'medium' }}>
+                {resource && resource.label}
+              </Text>
+
+              <Text as="div">{resource && resource.description}</Text>
+              <Box pad={{ vertical: 'small' }}>
+                <Text as="div" style={{ fontSize: 12 }}>
+                  added by {resource && resource.authorUsername}
+                </Text>
+                <Text as="div" style={{ fontSize: 12 }}>
+                  {moment(resource.creationDate).format('Do MMM YYYY')} <br />
+                </Text>
+              </Box>
+            </div>
+          )}
+        </NiceList>
+
+        {showModal && (
+          <Layer
+            onEsc={() => setShowModal(false)}
+            onClickOutside={() => setShowModal(false)}
+          >
+            <Box width="medium" pad="medium">
+              <Form
+                value={modalContent}
+                onChange={(nextValue) => setModalContent(nextValue)}
+                onSubmit={handleSubmit}
+              >
+                <FormField
+                  label="Name"
+                  margin={{ bottom: 'medium', top: 'medium' }}
+                >
+                  <TextInput
+                    plain={false}
+                    name="label"
+                    placeholder="Sound studio..."
+                  />
+                </FormField>
+
+                <FormField
+                  label="Description"
+                  margin={{ bottom: 'medium', top: 'medium' }}
+                >
+                  <TextArea
+                    plain={false}
+                    name="description"
+                    placeholder="Using studio requires care..."
+                  />
+                </FormField>
+
+                <Box direction="row" justify="end" pad="small">
+                  <Button type="submit" primary label="Confirm" />
+                </Box>
+              </Form>
+              <Box direction="row" justify="center" pad="small">
+                <Button
+                  secondary
+                  label="close"
+                  size="small"
+                  onClick={() => setShowModal(false)}
+                />
+              </Box>
+            </Box>
+          </Layer>
+        )}
+      </Box>
+    </Template>
+  );
+}
+
+export default Resources;
---
+++ server/methods/resources-methods.js
@@ -0,0 +1,85 @@
+import { Meteor } from 'meteor/meteor';
+import { getHost, isContributorOrAdmin } from './shared';
+
+Meteor.methods({
+  getResources() {
+    const host = getHost(this);
+
+    return Resources.find({ host }).fetch();
+  },
+
+  createResource(values) {
+    const user = Meteor.user();
+    const host = getHost(this);
+    const currentHost = Hosts.findOne({ host });
+    const resources = Resources.find({ host }).fetch();
+    if (resources.some((resource) => resource.label === values.label)) {
+      throw new Meteor.Error('There already is a resource with this name');
+    }
+    if (values.label.length < 3) {
+      throw new Meteor.Error(
+        'Resource name is too short. Minimum 3 letters required'
+      );
+    }
+
+    if (!user || !isContributorOrAdmin(user, currentHost)) {
+      throw new Meteor.Error('You are not allowed');
+    }
+
+    try {
+      const newResourceId = Resources.insert({
+        ...values,
+        host,
+        authorId: user._id,
+        authorAvatar: user.avatar || '',
+        authorUsername: user.username,
+        authorFirstName: user.firstName,
+        authorLastName: user.lastName,
+        creationDate: new Date(),
+      });
+      return newResourceId;
+    } catch (error) {
+      console.log(error);
+      throw new Meteor.Error(error);
+    }
+  },
+
+  updateResource(resourceId, values) {
+    const user = Meteor.user();
+    const host = getHost(this);
+    const currentHost = Hosts.findOne({ host });
+
+    if (!user || !isContributorOrAdmin(user, currentHost)) {
+      throw new Meteor.Error('You are not allowed');
+    }
+    try {
+      Resources.update(resourceId, {
+        $set: {
+          ...values,
+          updatedBy: user.username,
+          latestUpdate: new Date(),
+        },
+      });
+      return values.label;
+    } catch (error) {
+      console.log(error);
+      throw new Meteor.Error(error, "Couldn't add to Collection");
+    }
+  },
+
+  deleteResource(resourceId) {
+    const user = Meteor.user();
+    const host = getHost(this);
+    const currentHost = Hosts.findOne({ host });
+
+    if (!user || !isContributorOrAdmin(user, currentHost)) {
+      throw new Meteor.Error('You are not allowed');
+    }
+
+    try {
+      Resources.remove(resourceId);
+    } catch (error) {
+      throw new Meteor.Error(error, "Couldn't remove from collection");
+    }
+  },
+});
