3e2870eb8fa5cbe9bd27b1ee3a5541affc28e34a
Emin Durak <emin.tuta.io>
2020-12-02 18:27:17 +0300

    moved work component to a dedicated reusable component

--- client/RouterComponents/works/Works.jsx
+++ client/RouterComponents/works/Works.jsx
@@ -1,9 +1,9 @@
 import React, { useState, useEffect, useContext } from 'react';
 import { Link } from 'react-router-dom';
-import { LazyLoadImage } from 'react-lazy-load-image-component';
-import { Avatar, Box, Button, Text } from 'grommet';
+import { Box, Button } from 'grommet';
 import { FormAdd } from 'grommet-icons';
 
+import Work from '../../UIComponents/Work';
 import { StateContext } from '../../LayoutContainer';
 import Loader from '../../UIComponents/Loader';
 import Tag from '../../UIComponents/Tag';
@@ -14,18 +14,6 @@ const compareByDate = (a, b) => {
   const dateA = new Date(a.creationDate);
   const dateB = new Date(b.creationDate);
   return dateB - dateA;
-};
-
-const imageStyle = {
-  width: '100%',
-  height: 220,
-  objectFit: 'cover',
-};
-
-const ellipsisStyle = {
-  whiteSpace: 'nowrap',
-  overflow: 'hidden',
-  textOverflow: 'ellipsis',
 };
 
 function getHSL(length, index, opacity = 1) {
@@ -112,54 +100,6 @@ const Works = ({ history }) => {
     </Box>
   );
 };
-
-function Work({ work, history }) {
-  return (
-    <Box
-      key={work._id}
-      onClick={() => history.push(`/${work.authorUsername}/work/${work._id}`)}
-      basis="360px"
-      margin="small"
-      round="3px"
-      hoverIndicator="brand-light"
-      style={{ background: 'rgba(255, 255, 255, .6)' }}
-    >
-      <Box pad="small" direction="row" justify="between">
-        <Box>
-          {work.category && (
-            <Tag
-              label={work.category.label}
-              background={work.category.color}
-              margin={{ bottom: 'xxsmall' }}
-            />
-          )}
-          <Text weight={600} style={ellipsisStyle} size="large">
-            {work.title}
-          </Text>
-        </Box>
-        <Avatar
-          flex={{ grow: 0 }}
-          src={work.authorAvatar && work.authorAvatar.src}
-        />
-      </Box>
-      {work.images && work.images[0] && (
-        <Box>
-          <LazyLoadImage
-            alt={work.title}
-            src={work.images[0]}
-            style={imageStyle}
-            effect="black-and-white"
-          />
-        </Box>
-      )}
-      <Box pad="small">
-        <Text weight={300} style={ellipsisStyle}>
-          {work.shortDescription}
-        </Text>
-      </Box>
-    </Box>
-  );
-}
 
 getCategories = (works) => {
   const labels = Array.from(
---
+++ client/UIComponents/Work.jsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import { Avatar, Box, Text } from 'grommet';
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+import Tag from './Tag';
+
+const imageStyle = {
+  width: '100%',
+  height: 220,
+  objectFit: 'cover',
+};
+
+const ellipsisStyle = {
+  whiteSpace: 'nowrap',
+  overflow: 'hidden',
+  textOverflow: 'ellipsis',
+};
+
+function Work({ work, history }) {
+  return (
+    <Box
+      key={work._id}
+      onClick={() => history.push(`/${work.authorUsername}/work/${work._id}`)}
+      basis="360px"
+      margin="small"
+      round="3px"
+      hoverIndicator="brand-light"
+      style={{ background: 'rgba(255, 255, 255, .6)' }}
+    >
+      <Box pad="small" direction="row" justify="between">
+        <Box>
+          {work.category && (
+            <Tag
+              label={work.category.label}
+              background={work.category.color}
+              margin={{ bottom: 'xxsmall' }}
+            />
+          )}
+          <Text weight={600} style={ellipsisStyle} size="large">
+            {work.title}
+          </Text>
+        </Box>
+        <Avatar
+          flex={{ grow: 0 }}
+          src={work.authorAvatar && work.authorAvatar.src}
+        />
+      </Box>
+      {work.images && work.images[0] && (
+        <Box>
+          <LazyLoadImage
+            alt={work.title}
+            src={work.images[0]}
+            style={imageStyle}
+            effect="black-and-white"
+          />
+        </Box>
+      )}
+      <Box pad="small">
+        <Text weight={300} style={ellipsisStyle}>
+          {work.shortDescription}
+        </Text>
+      </Box>
+    </Box>
+  );
+}
+
+export default Work;
