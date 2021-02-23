1ad83d8f365cd4f136d89f012c67f1135ce141ab
Emin Durak <emin.tuta.io>
2021-02-23 16:01:28 +0300

    reverting back

--- server/main.js
+++ server/main.js
@@ -18,19 +18,4 @@ Meteor.startup(() => {
     url = url.replace('#/', '');
     return `To reset your password, simply click the link below. ${url}`;
   };
-
-  Meteor.users.find().forEach((user) => {
-    Meteor.users.update(
-      { _id: user._id },
-      {
-        $push: {
-          memberships: {
-            host: 'circles.market',
-            role: user.memberships[0].role,
-            date: user.memberships[0].date,
-          },
-        },
-      }
-    );
-  });
 });
