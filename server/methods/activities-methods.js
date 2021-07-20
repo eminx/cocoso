fb4be84358e99cfeec7f98083fcf830f50f4dece
Emin Durak <emin.tuta.io>
2021-07-20 13:43:54 +0300

    emails better sent

--- server/methods/activities-methods.js
+++ server/methods/activities-methods.js
@@ -1,35 +1,29 @@
 import { Meteor } from 'meteor/meteor';
-import {
-  getHost,
-  isContributorOrAdmin,
-  isParticipant,
-  isAdmin,
-  isMember,
-} from './shared';
-
-import { getResourceIndex, siteUrl } from './shared';
+import { getHost, isContributorOrAdmin } from './shared';
 
 const getRegistrationEmailBody = (
   firstName,
   numberOfPeople,
   occurence,
   activityId,
-  hostName
+  hostName,
+  host
 ) => {
-  return `Hi ${firstName},\n\nThis is a confirmation email to inform you that you have successfully signed up for this event.\nYou have registered to come ${numberOfPeople} ${
+  return `<p>Hi ${firstName},</p><p>This is a confirmation email to inform you that you have successfully signed up for this event.</p><p>You have registered to come ${numberOfPeople} ${
     numberOfPeople === 1 ? 'person' : 'people'
   } in total for the event on ${occurence.startDate} at ${
     occurence.startTime
-  }.\nMay there be any changes to that, please go to this link to change your RSVP: ${siteUrl}event/${activityId}.\nThen by opening the date you signed up for, click the "Change RSVP" link and follow the instructions there.\nWe look forward to your participation.\n\n${hostName} Team`;
+  }.</p><p>May there be any changes to that, please go to this link to change your RSVP: https://${host}/event/${activityId}.<br />Then by opening the date you signed up for, click the "Change RSVP" link and follow the instructions there.</p><p>We look forward to your participation.<br />${hostName} Team</p>`;
 };
 
 const getUnregistrationEmailBody = (
   firstName,
   occurence,
   activityId,
-  hostName
+  hostName,
+  host
 ) => {
-  return `Hi ${firstName},\n\nThis is a confirmation email to inform you that you have successfully removed your registration from this event.\nYou have previously registered to attend the event on ${occurence.startDate} at ${occurence.startTime}, which you just signed out of. \nIf you want to RSVP again, you can do so here at the event page: ${siteUrl}event/${activityId}.\n\nKind regards,\n${hostName} Team`;
+  return `<p>Hi ${firstName},</p><p>This is a confirmation email to inform you that you have successfully removed your registration from this event.</p><p>You have previously registered to attend the event on ${occurence.startDate} at ${occurence.startTime}, which you just signed out of.</p><p>If you want to RSVP again, you can do so here at the event page: https://${host}/event/${activityId}.</p><p>Kind regards,<br />${hostName} Team</p>`;
 };
 
 Meteor.methods({
@@ -227,7 +221,8 @@ Meteor.methods({
           values.numberOfPeople,
           occurences[occurenceIndex],
           activityId,
-          hostName
+          hostName,
+          host
         )
       );
     } catch (error) {
@@ -269,7 +264,8 @@ Meteor.methods({
           values.numberOfPeople,
           occurences[occurenceIndex],
           activityId,
-          hostName
+          hostName,
+          host
         )
       );
     } catch (error) {
@@ -311,7 +307,8 @@ Meteor.methods({
           theNonAttendee.firstName,
           theOccurence,
           activityId,
-          hostName
+          hostName,
+          host
         )
       );
     } catch (error) {
