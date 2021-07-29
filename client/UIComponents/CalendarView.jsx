837f5a07b18bd2d66a3fddd4177e0ede6703b487
Emin Durak <emin.tuta.io>
2021-07-29 18:33:04 +0300

    removed unused code

--- client/UIComponents/CalendarView.jsx
+++ client/UIComponents/CalendarView.jsx
@@ -1,8 +1,5 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import BigCalendar from 'react-big-calendar';
-
-let allViews = Object.keys(BigCalendar.Views).map((k) => BigCalendar.Views[k]);
 
 const CalendarView = (props) => {
   const { activities } = props;
@@ -31,81 +28,3 @@ const CalendarView = (props) => {
 };
 
 export default CalendarView;
-
-class CalendarToolbar extends React.Component {
-  static propTypes = {
-    view: PropTypes.string.isRequired,
-    views: PropTypes.arrayOf(PropTypes.string).isRequired,
-    label: PropTypes.node.isRequired,
-    localizer: PropTypes.object,
-    onNavigate: PropTypes.func.isRequired,
-  };
-
-  render() {
-    let {
-      localizer: { messages },
-      label,
-    } = this.props;
-
-    return (
-      <div className="rbc-toolbar">
-        <span className="rbc-btn-process">
-          {this.viewNamesProcess(messages)}
-        </span>
-
-        <span
-          className="rbc-toolbar-label"
-          style={{ textTransform: 'uppercase', color: '#ea3924' }}
-        >
-          {label}
-        </span>
-
-        <span className="rbc-btn-process">
-          <a onClick={this.navigate.bind(null, navigate.TODAY)}>
-            {messages.today}
-          </a>
-          <a onClick={this.navigate.bind(null, navigate.PREVIOUS)}>
-            <Icon
-              // theme="outlined"
-              type="double-left"
-              style={{ fontSize: 24, cursor: 'pointer' }}
-            />
-          </a>
-          <a onClick={this.navigate.bind(null, navigate.NEXT)}>
-            <Icon
-              // theme="outlined"
-              type="double-right"
-              style={{ fontSize: 24, cursor: 'pointer' }}
-            />
-          </a>
-        </span>
-      </div>
-    );
-  }
-
-  navigate = (action) => {
-    this.props.onNavigate(action);
-  };
-
-  view = (view) => {
-    this.props.onView(view);
-  };
-
-  viewNamesProcess(messages) {
-    let viewNames = this.props.views;
-    const view = this.props.view;
-
-    if (viewNames.length > 1) {
-      return viewNames.map((name) => (
-        <a
-          type="button"
-          key={name}
-          className={cn({ 'rbc-active': view === name })}
-          onClick={this.view.bind(null, name)}
-        >
-          {messages[name]}
-        </a>
-      ));
-    }
-  }
-}
