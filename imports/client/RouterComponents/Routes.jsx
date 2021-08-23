51ba354d26d5856bafb4699b2dca7baa2902770b
Emin Durak <emin.tuta.io>
2021-08-23 15:52:21 +0300

    lazy loading routes

--- imports/client/RouterComponents/Routes.jsx
+++ imports/client/RouterComponents/Routes.jsx
@@ -8,57 +8,72 @@ import LayoutContainer from '../LayoutContainer';
 // ROUTES
 
 // Calendar
-import CalendarContainer from './CalendarContainer';
+const CalendarContainer = lazy(() => import('./CalendarContainer'));
 
 // Activities
-import ActivitiesContainer from './activities/ActivitiesContainer';
+const ActivitiesContainer = lazy(() =>
+  import('./activities/ActivitiesContainer')
+);
 
-import ActivityContainer from './activities/ActivityContainer';
-import MyActivities from './activities/MyActivities';
-import NewActivityContainer from './activities/NewActivityContainer';
+const ActivityContainer = lazy(() => import('./activities/ActivityContainer'));
+const MyActivities = lazy(() => import('./activities/MyActivities'));
+const NewActivityContainer = lazy(() =>
+  import('./activities/NewActivityContainer')
+);
 
-import EditActivityContainer from './activities/EditActivityContainer';
+const EditActivityContainer = lazy(() =>
+  import('./activities/EditActivityContainer')
+);
 
 // Processes
-import NewProcessContainer from './processes/NewProcessContainer';
+const NewProcessContainer = lazy(() =>
+  import('./processes/NewProcessContainer')
+);
 
-import EditProcessContainer from './processes/EditProcessContainer';
+const EditProcessContainer = lazy(() =>
+  import('./processes/EditProcessContainer')
+);
 
-import ProcessesListContainer from './processes/ProcessesListContainer';
+const ProcessesListContainer = lazy(() =>
+  import('./processes/ProcessesListContainer')
+);
 
-import ProcessContainer from './processes/ProcessContainer';
+const ProcessContainer = lazy(() => import('./processes/ProcessContainer'));
 
 // Pages
-import Page from './pages/Page';
-import NewPageContainer from './pages/NewPageContainer';
-import EditPageContainer from './pages/EditPageContainer';
+const Page = lazy(() => import('./pages/Page'));
+const NewPageContainer = lazy(() => import('./pages/NewPageContainer'));
+const EditPageContainer = lazy(() => import('./pages/EditPageContainer'));
 
 // Works
-import Works from './works/Works';
-import Work from './works/Work';
-import MyWorks from './works/MyWorks';
-import NewWork from './works/NewWork';
-import EditWork from './works/EditWork';
+const Works = lazy(() => import('./works/Works'));
+const Work = lazy(() => import('./works/Work'));
+const MyWorks = lazy(() => import('./works/MyWorks'));
+const NewWork = lazy(() => import('./works/NewWork'));
+const EditWork = lazy(() => import('./works/EditWork'));
 
 // Members
-import ProfileContainer from './profile/ProfileContainer';
-import MembersPublic from './members/MembersPublic';
-import MemberPublic from './members/Member';
-import SignupPage from '../account-manager/SignupPage';
-import LoginPage from '../account-manager/LoginPage';
-import ForgotPasswordPage from '../account-manager/ForgotPasswordPage';
-
-import ResetPasswordPage from '../account-manager/ResetPasswordPage';
+const ProfileContainer = lazy(() => import('./profile/ProfileContainer'));
+const MembersPublic = lazy(() => import('./members/MembersPublic'));
+const MemberPublic = lazy(() => import('./members/Member'));
+const SignupPage = lazy(() => import('../account-manager/SignupPage'));
+const LoginPage = lazy(() => import('../account-manager/LoginPage'));
+const ForgotPasswordPage = lazy(() =>
+  import('../account-manager/ForgotPasswordPage')
+);
+const ResetPasswordPage = lazy(() =>
+  import('../account-manager/ResetPasswordPage')
+);
 
 // admin
-import Settings from './admin/Settings';
-import Members from './admin/Members';
-import Resources from './admin/Resources';
+const Settings = lazy(() => import('./admin/Settings'));
+const Members = lazy(() => import('./admin/Members'));
+const Resources = lazy(() => import('./admin/Resources'));
 
 // superadmin
-import NewHost from './hosts/NewHost';
+const NewHost = lazy(() => import('./hosts/NewHost'));
 
-import NotFoundPage from './NotFoundPage';
+const NotFoundPage = lazy(() => import('./NotFoundPage'));
 
 const browserHistory = createBrowserHistory();
 
@@ -67,56 +82,66 @@ export default function () {
     <Router history={browserHistory}>
       <Switch>
         <LayoutContainer history={browserHistory}>
-          {/* <Suspense fallback={<div>loading the page...</div>}> */}
-          <Route exact path="/" component={Home} />
+          <Suspense fallback={<div>loading the page...</div>}>
+            <Route exact path="/" component={Home} />
 
-          <Route exact path="/calendar" component={CalendarContainer} />
+            <Route exact path="/calendar" component={CalendarContainer} />
 
-          <Route exact path="/activities" component={ActivitiesContainer} />
-          <Route exact path="/my-activities" component={MyActivities} />
-          <Route exact path="/new-activity" component={NewActivityContainer} />
-          <Route path="/event/:id" component={ActivityContainer} />
-          <Route path="/activity/:id" component={ActivityContainer} />
-          <Route path="/edit-activity/:id/" component={EditActivityContainer} />
+            <Route exact path="/activities" component={ActivitiesContainer} />
+            <Route exact path="/my-activities" component={MyActivities} />
+            <Route
+              exact
+              path="/new-activity"
+              component={NewActivityContainer}
+            />
+            <Route path="/event/:id" component={ActivityContainer} />
+            <Route path="/activity/:id" component={ActivityContainer} />
+            <Route
+              path="/edit-activity/:id/"
+              component={EditActivityContainer}
+            />
 
-          <Route exact path="/new-process" component={NewProcessContainer} />
-          <Route path="/processes/" component={ProcessesListContainer} />
-          <Route path="/process/:id" component={ProcessContainer} />
-          <Route path="/edit-process/:id/" component={EditProcessContainer} />
+            <Route exact path="/new-process" component={NewProcessContainer} />
+            <Route path="/processes/" component={ProcessesListContainer} />
+            <Route path="/process/:id" component={ProcessContainer} />
+            <Route path="/edit-process/:id/" component={EditProcessContainer} />
 
-          <Route path="/members" component={MembersPublic} />
-          <Route path="/@:username" component={MemberPublic} />
+            <Route path="/members" component={MembersPublic} />
+            <Route path="/@:username" component={MemberPublic} />
 
-          <Route exact path="/new-page" component={NewPageContainer} />
-          <Route path="/page/:id" component={Page} />
-          <Route path="/edit-page/:id/" component={EditPageContainer} />
+            <Route exact path="/new-page" component={NewPageContainer} />
+            <Route path="/page/:id" component={Page} />
+            <Route path="/edit-page/:id/" component={EditPageContainer} />
 
-          <Route
-            path="/my-profile/"
-            history={browserHistory}
-            component={ProfileContainer}
-          />
+            <Route
+              path="/my-profile/"
+              history={browserHistory}
+              component={ProfileContainer}
+            />
 
-          <Route path="/my-works" component={MyWorks} />
-          <Route path="/:username/work/:workId" component={Work} />
-          <Route path="/:username/edit-work/:workId" component={EditWork} />
-          <Route path="/new-work" component={NewWork} />
-          <Route path="/works" component={Works} />
+            <Route path="/my-works" component={MyWorks} />
+            <Route path="/:username/work/:workId" component={Work} />
+            <Route path="/:username/edit-work/:workId" component={EditWork} />
+            <Route path="/new-work" component={NewWork} />
+            <Route path="/works" component={Works} />
 
-          <Route path="/admin/settings" component={Settings} />
-          <Route path="/admin/members" component={Members} />
-          <Route path="/admin/resources" component={Resources} />
+            <Route path="/admin/settings" component={Settings} />
+            <Route path="/admin/members" component={Members} />
+            <Route path="/admin/resources" component={Resources} />
 
-          <Route path="/signup" component={SignupPage} />
-          <Route path="/login" component={LoginPage} />
-          <Route path="/forgot-password" component={ForgotPasswordPage} />
-          <Route path="/reset-password/:token" component={ResetPasswordPage} />
+            <Route path="/signup" component={SignupPage} />
+            <Route path="/login" component={LoginPage} />
+            <Route path="/forgot-password" component={ForgotPasswordPage} />
+            <Route
+              path="/reset-password/:token"
+              component={ResetPasswordPage}
+            />
 
-          {/* <Route path="*" component={NotFoundPage} /> */}
+            {/* <Route path="*" component={NotFoundPage} /> */}
 
-          {/* Super admin only! */}
-          <Route path="/new-host" component={NewHost} />
-          {/* </Suspense> */}
+            {/* Super admin only! */}
+            <Route path="/new-host" component={NewHost} />
+          </Suspense>
         </LayoutContainer>
       </Switch>
     </Router>
