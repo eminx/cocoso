24f60521f0babaa9bf49499544f2c5ec92ffb976
Emin Durak <emin@tuta.io>
2021-12-10 00:32:46 +0300

    better semantic with link instead of history

--- imports/client/UIComponents/UserPopup.jsx
+++ imports/client/UIComponents/UserPopup.jsx
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react';
-import { withRouter } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import {
   Avatar,
   Box,
@@ -15,17 +15,15 @@ import {
 import { StateContext } from '../LayoutContainer';
 import { userMenu, adminMenu } from '../constants/general';
 
-const UserPopup = withRouter(({ currentUser, history }) => {
+function UserPopup({ currentUser }) {
   if (!currentUser) {
     return (
       <Box px="1">
-        <Button
-          variant="ghost"
-          size="sm"
-          onClick={() => history.push('/login')}
-        >
-          Login
-        </Button>
+        <Link to="/login">
+          <Button as="span" size="sm" variant="ghost">
+            Login
+          </Button>
+        </Link>
       </Box>
     );
   }
@@ -44,27 +42,24 @@ const UserPopup = withRouter(({ currentUser, history }) => {
       <MenuList>
         <MenuGroup title="My">
           {userMenu.map((item) => (
-            <MenuItem key={item.label} onClick={() => history.push(item.value)}>
-              {item.label}
-            </MenuItem>
+            <Link key={item.label} to={item.value}>
+              <MenuItem>{item.label}</MenuItem>
+            </Link>
           ))}
         </MenuGroup>
         {role === 'admin' && <MenuDivider />}
         {role === 'admin' && (
           <MenuGroup title="Admin">
             {adminMenu.map((item) => (
-              <MenuItem
-                key={item.label}
-                onClick={() => history.push(item.value)}
-              >
-                {item.label}
-              </MenuItem>
+              <Link key={item.label} to={item.value}>
+                <MenuItem>{item.label}</MenuItem>
+              </Link>
             ))}
           </MenuGroup>
         )}
       </MenuList>
     </Menu>
   );
-});
+}
 
 export default UserPopup;
