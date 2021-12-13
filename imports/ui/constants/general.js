a824f0710e8f7da60e059f64491bc01bba3ba5d8
ig <ilkergonenc@gmail.com>
2021-12-13 17:39:02 +0300

    merged lib/constants in to ui/constants/general file

--- imports/ui/constants/general.js
+++ imports/ui/constants/general.js
@@ -11,4 +11,115 @@ const adminMenu = [
   { label: 'Emails', value: '/admin/emails' },
 ];
 
-export { userMenu, adminMenu };
+const hostFields = [
+  {
+    label: 'desired url/address',
+    placeholder: 'pineapples.fanus.co',
+    name: 'host',
+    isRequired: true,
+  },
+  {
+    label: 'name',
+    placeholder: 'Pineapples',
+    name: 'name',
+    isRequired: true,
+  },
+  {
+    label: 'contact email',
+    placeholder: 'email@pineapple.com',
+    name: 'email',
+    isRequired: true,
+  },
+  {
+    label: 'address',
+    placeholder: 'Paul Klee Boulevard 99',
+    name: 'address',
+  },
+  {
+    label: 'city',
+    placeholder: 'Pushkar',
+    name: 'city',
+  },
+  {
+    label: 'country',
+    placeholder: 'Galaxy Land',
+    name: 'country',
+  },
+  {
+    label: 'about',
+    placeholder: 'About your hub',
+    name: 'about',
+    textArea: true,
+    isRequired: true,
+  },
+];
+
+const defaultMenu = [
+  {
+    label: 'Activities',
+    name: 'activities',
+    isVisible: true,
+    isHomePage: true,
+  },
+  {
+    label: 'Processes',
+    name: 'processes',
+    isVisible: true,
+    isHomePage: false,
+  },
+  {
+    label: 'Calendar',
+    name: 'calendar',
+    isVisible: true,
+    isHomePage: false,
+  },
+  {
+    label: 'Works',
+    name: 'works',
+    isVisible: true,
+    isHomePage: false,
+  },
+  {
+    label: 'Members',
+    name: 'members',
+    isVisible: true,
+    isHomePage: false,
+  },
+  {
+    label: 'Info',
+    name: 'info',
+    isVisible: true,
+    isHomePage: false,
+  },
+];
+
+const defaultMainColor = {
+  hsl: {
+    h: '144',
+    s: 0.8,
+    l: 0.35,
+  },
+};
+
+const defaultEmails = [
+  {
+    title: 'Welcome Email',
+    subject: 'Welcome as a participant',
+    appeal: 'Dear',
+    body: 'Welcome! As a participant, you may attend our processes and activities. Look forward to hanging out with you!',
+  },
+  {
+    title: 'New Contributor',
+    subject: 'You are now a contributor',
+    appeal: 'Dear',
+    body: 'You are now verified to be a contributor in our space. This means that you are privileged to create public activities & processes, publish your works, and book resources in our community. \nLook forward!',
+  },
+  {
+    title: 'New Admin',
+    subject: 'You are now an admin',
+    appeal: 'Dear',
+    body: 'You are now registered as an admin in our space. This means that in addition to the privileges of being a contributor; you may create new pages and configure a bunch of options in our shared system. Please use your powers responsible, and enjoy. \nLook forward!',
+  },
+];
+
+export { userMenu, adminMenu, hostFields, defaultMenu, defaultMainColor, defaultEmails };
