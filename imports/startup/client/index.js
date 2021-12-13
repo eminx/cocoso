bb821f2fe53db3579c509cafd26cbfa0cb17efb7
ig <ilkergonenc@gmail.com>
2021-12-13 18:22:05 +0300

    fix import url

--- imports/startup/client/index.js
+++ imports/startup/client/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
 
-import App from '../ui/App.jsx';
+import App from '../../ui/App';
 
 Meteor.startup(() => {
   render(<App />, document.getElementById('render-target'));
