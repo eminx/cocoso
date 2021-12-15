6f9a1037ff8edeca1d8d5d24cbce41f1059cb22e
ig <ilkergonenc@gmail.com>
2021-12-15 14:06:29 +0300

    update imports for pages folder

--- imports/startup/server/api.js
+++ imports/startup/server/api.js
@@ -1,3 +1,9 @@
+import '../../api/@/shared';
+import '../../api/pages/page';
+import '../../api/pages/pages.methods';
+import '../../api/pages/server/pages.publications';
+
+import '../../api/methods/shared';
 import '../../api/accounts/accounts-general';
 import '../../api/methods/activities-methods';
 import '../../api/methods/admin-methods';
@@ -6,10 +12,9 @@ import '../../api/methods/document-methods';
 import '../../api/methods/email-methods';
 import '../../api/methods/host-methods';
 import '../../api/methods/image-upload';
-import '../../api/methods/page-methods';
+// import '../../api/methods/page-methods';
 import '../../api/methods/process-methods';
 import '../../api/methods/profile-methods';
-import '../../api/methods/publish';
 import '../../api/methods/resources-methods';
-import '../../api/methods/shared';
 import '../../api/methods/work-methods';
+import '../../api/methods/publish';
