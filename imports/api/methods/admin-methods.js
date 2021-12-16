9e9c0a562fe90ade962ca8ad3be2d76ad88e0215
ig <ilkergonenc@gmail.com>
2021-12-16 11:23:46 +0300

    delete categories methods and related imports and consts

--- imports/api/methods/admin-methods.js
+++ imports/api/methods/admin-methods.js
@@ -6,25 +6,11 @@ import {
   getHost,
   isContributorOrAdmin,
   isContributor,
-  isMember,
 } from './shared';
 
 const getVerifiedEmailText = (username) => {
   return `Hi ${username},\n\nWe're very happy to inform you that you are now a verified member at ${publicSettings.name}.\n\nThis means that from now on you're welcome to create your own study processes and book spaces & tools either for your own projects or to make a public event. We would like to encourage you to use this tool and wish you to keep a good collaboration with your team.\n\nKind regards,\n${publicSettings.name} Team`;
 };
-
-const catColors = [
-  'hsla(10, 62%, 80%, 0.7)',
-  'hsla(46, 62%, 80%, 0.7)',
-  'hsla(82, 62%, 80%, 0.7)',
-  'hsla(118, 62%, 80%, 0.7)',
-  'hsla(154, 62%, 80%, 0.7)',
-  'hsla(190, 62%, 80%, 0.7)',
-  'hsla(226, 62%, 80%, 0.7)',
-  'hsla(262, 62%, 80%, 0.7)',
-  'hsla(298, 62%, 80%, 0.7)',
-  'hsla(334, 62%, 80%, 0.7)',
-];
 
 const isUserAdmin = (members, userId) => {
   return members.some(
@@ -233,66 +219,6 @@ Meteor.methods({
       throw new Meteor.Error(error);
     }
   },
-
-  // getCategories() {
-  //   const user = Meteor.user();
-  //   const host = getHost(this);
-  //   if (!user) {
-  //     throw new Meteor.Error('You are not allowed');
-  //   }
-
-  //   return Categories.find({
-  //     host,
-  //   }).fetch();
-  // },
-
-  // addNewCategory(category, type) {
-  //   const user = Meteor.user();
-  //   const host = getHost(this);
-  //   const currentHost = Hosts.findOne({ host });
-  //   const isAdmin = currentHost && isUserAdmin(currentHost.members, user._id);
-
-  //   if (!user.isSuperAdmin && !isAdmin) {
-  //     throw new Meteor.Error('You are not allowed');
-  //   }
-
-  //   if (Categories.findOne({ label: category.toLowerCase(), host })) {
-  //     throw new Meteor.Error('Category already exists!');
-  //   }
-
-  //   const catLength = Categories.find({ host, type }).count();
-
-  //   try {
-  //     return Categories.insert({
-  //       host,
-  //       type,
-  //       label: category.toLowerCase(),
-  //       color: catColors[catLength],
-  //       addedBy: user._id,
-  //       addedUsername: user.username,
-  //       addedDate: new Date(),
-  //     });
-  //   } catch (error) {
-  //     throw new Meteor.Error(error);
-  //   }
-  // },
-
-  // removeCategory(categoryId) {
-  //   const user = Meteor.user();
-  //   const host = getHost(this);
-  //   const currentHost = Hosts.findOne({ host });
-  //   const isAdmin = currentHost && isUserAdmin(currentHost.members, user._id);
-
-  //   if (!user.isSuperAdmin && !isAdmin) {
-  //     throw new Meteor.Error('You are not allowed');
-  //   }
-
-  //   try {
-  //     Categories.remove(categoryId);
-  //   } catch (error) {
-  //     throw new Meteor.Error(error);
-  //   }
-  // },
 
   assignHostLogo(image) {
     const user = Meteor.user();
