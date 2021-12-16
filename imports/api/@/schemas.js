d88ee81f0735aff0e9acf382f36b7b17b84318e5
ig <ilkergonenc@gmail.com>
2021-12-16 11:22:13 +0300

    add some common schemas to shared schemas file

--- imports/api/@/schemas.js
+++ imports/api/@/schemas.js
@@ -1,9 +1,24 @@
-const Schemas = {};
+import SimpleSchema from 'simpl-schema';
 
 const CustomValidators = {
   RegEx: {
     Hostname: "^(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\-]*[A-Za-z0-9])$",
   }
 };
+
+const Schemas = {};
+
+Schemas.Host = {
+  type: String, 
+  regEx: CustomValidators.RegEx.Hostname
+};
+Schemas.Avatar = {
+  src: {
+    type: String, 
+    regEx: SimpleSchema.RegEx.Url
+  },
+  date: {type: Date},
+};
+
 
 export { Schemas, CustomValidators };
