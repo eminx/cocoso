4305bdbf4bc3f6da0a23915cdb4cb2b2e1299b8d
ig <ilkergonenc@gmail.com>
2021-12-18 12:01:57 +0300

    remove host and user publications from shared publishes

--- imports/api/@/publish.js
+++ imports/api/@/publish.js
@@ -1,16 +1,6 @@
 import { Meteor } from 'meteor/meteor';
 import { getHost, isContributorOrAdmin } from './shared';
 
-Meteor.publish('attendingEvents', function () {
-  return Meteor.users.find(this.userId, {
-    fields: {
-      attending: 1,
-      profile: 1,
-      isSuperAdmin: 1,
-    },
-  });
-});
-
 Meteor.publish('activities', function (onlyPublic = false) {
   const host = getHost(this);
   // const fields = {
@@ -91,8 +81,6 @@ Meteor.publish('process', function (id) {
   });
 });
 
-
-
 Meteor.publish('work', function (id) {
   return Works.find({
     _id: id,
@@ -127,53 +115,6 @@ Meteor.publish('resources', function () {
 
 Meteor.publish('documents', function () {
   return Documents.find();
-});
-
-Meteor.publish('me', function () {
-  const userId = Meteor.userId();
-  if (userId) {
-    return Meteor.users.find(userId);
-  }
-});
-
-Meteor.publish('currentHost', function () {
-  const host = getHost(this);
-  return Hosts.find({ host }, { fields: { settings: true, logo: true } });
-});
-
-Meteor.publish('members', function () {
-  const user = Meteor.user();
-  const host = getHost(this);
-  const currentHost = Hosts.findOne({ host });
-
-  if (user.isSuperAdmin || isContributorOrAdmin(user, currentHost)) {
-    return Hosts.find({ host });
-  }
-});
-
-Meteor.publish('membersForPublic', function () {
-  const host = getHost(this);
-  // Meteor.users._ensureIndex({ 'memberships.host': host });
-  return Meteor.users.find(
-    { 'memberships.host': host },
-    {
-      fields: {
-        _id: true,
-        username: true,
-        avatar: true,
-        firstName: true,
-        lastName: true,
-      },
-    }
-  );
-});
-
-Meteor.publish('memberAtHost', function (username) {
-  const host = getHost(this);
-  return Meteor.users.find({
-    username,
-    'memberships.host': host,
-  });
 });
 
 Meteor.publish('memberWorksAtHost', function (username) {
