4c7c6371b7ef2b2407f158f2b7a9d2c1b4fee2fd
ig <ilkergonenc@gmail.com>
2021-12-23 12:01:37 +0300

    fix unused var and import

--- imports/api/chats/chat.publications.js
+++ imports/api/chats/chat.publications.js
@@ -1,9 +1,9 @@
 import { Meteor } from 'meteor/meteor';
-import { getHost } from '../@/shared';
+// import { getHost } from '../@/shared';
 import Chats from './chat';
 
 Meteor.publish('chat', function (contextId) {
-  const host = getHost(this);
+  // const host = getHost(this);
   const user = Meteor.user();
   if (user) {
     return Chats.find({
