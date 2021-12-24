bfed0380c71267ebf6639fed215ac2bd4ab46aa2
ig <ilkergonenc@gmail.com>
2021-12-24 13:37:30 +0300

    improve chats schema usages

--- imports/api/chats/chat.js
+++ imports/api/chats/chat.js
@@ -5,19 +5,19 @@ import { Schemas } from '../@/schemas';
 const Chats = new Mongo.Collection('chats');
 
 Schemas.Chats = new SimpleSchema({
-  _id: {type: String},
-  host: {type: String},
+  _id: Schemas.Id,
+  host: Schemas.Host,
 
-  contextId: {type: String},
+  contextId: Schemas.Id,
   contextName: {type: String},
 
   createdBy: {type: Object},
-  'createdBy.userId': {type: String},
+  'createdBy.userId': Schemas.Id,
   'createdBy.username': {type: String},
 
   messages: {type: Array, optional: true},
   'messages.$': {type: Object},
-  'messages.$.senderId': {type: String},
+  'messages.$.senderId': Schemas.Id,
   'messages.$.senderUsername': {type: String},
   'messages.$.content': {type: String},
   'messages.$.createdDate': {type: Date},
