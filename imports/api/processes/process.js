459c90b16d979006f31f8ab15975d6dfda9ed9ce
ig <ilkergonenc@gmail.com>
2021-12-26 12:29:29 +0300

    reshape process schema

--- imports/api/processes/process.js
+++ imports/api/processes/process.js
@@ -4,38 +4,6 @@ import { Schemas } from '../@/schemas';
 
 const Processes = new Mongo.Collection('processes');
 
-const SchemasProcesses = {
-  members: {
-    memberId: Schemas.Id,
-    username: { type: String },
-    profileImage: { type: String, regEx: SimpleSchema.RegEx.Url, optional: true },
-    isRegisteredMember: { type: Boolean, optional: true },
-    joinDate: { type: Date },
-  },
-  documents: {
-    name: { type: String },
-    downloadUrl: { type: String, regEx: SimpleSchema.RegEx.Url},
-  },
-  meetings: {
-    attendees: { type: Array },
-    'attendees.$': { type: Object, optional: true },
-    'attendees.$.memberId': Schemas.Id,
-    'attendees.$.memberUsername': { type: String },
-    'attendees.$.confirmDate': { type: Date },
-    startDate: { type: String },
-    startTime: { type: String },
-    endDate: { type: String },
-    endTime: { type: String },
-    room: { type: String, optional: true }, 
-    resource: { type: String, optional: true }, 
-    resourceIndex: { type: String, optional: true }, 
-  },
-  peopleInvited: {
-    email: { type: String, regEx: SimpleSchema.RegEx.Email },
-    firstName: { type: String },
-  },
-};
-
 Schemas.Processes = new SimpleSchema({
   _id: Schemas.Id,
   host: Schemas.Hostname,
@@ -50,13 +18,45 @@ Schemas.Processes = new SimpleSchema({
   capacity: { type: SimpleSchema.Integer },
 
   members: { type: Array },
-  'members.$': new SimpleSchema(SchemasProcesses.members),
-  documents: { type: Array, optional: true },
-  'documents.$': new SimpleSchema(SchemasProcesses.documents),
+  'members.$': new SimpleSchema({
+    memberId: Schemas.Id,
+    username: { type: String },
+    profileImage: { type: String, regEx: SimpleSchema.RegEx.Url, optional: true },
+    isRegisteredMember: { type: Boolean, optional: true },
+    joinDate: { type: Date },
+  }),
+
+  documents: { type: Array, defaultValue: [] },
+  'documents.$': { type: new SimpleSchema({
+    name: { type: String },
+    downloadUrl: { type: String, regEx: SimpleSchema.RegEx.Url},
+  }), optional: true },
+
   meetings: { type: Array, defaultValue: [] },
-  'meetings.$': { type: new SimpleSchema(SchemasProcesses.meetings), optional: true },
+  'meetings.$': { type: new SimpleSchema({
+    startDate: { type: String },
+    startTime: { type: String },
+    endDate: { type: String },
+    endTime: { type: String },
+
+    room: { type: String, optional: true }, 
+    resource: { type: String, optional: true }, 
+    resourceIndex: { type: String, optional: true }, 
+
+    attendees: { type: Array },
+    'attendees.$': { type: new SimpleSchema({
+      memberId: Schemas.Id,
+      memberUsername: { type: String },
+      confirmDate: { type: Date },
+    }), optional: true },
+
+  }), optional: true },
+
   peopleInvited: { type: Array, defaultValue: [] },
-  'peopleInvited.$': { type: new SimpleSchema(SchemasProcesses.peopleInvited), optional: true },
+  'peopleInvited.$': { type: new SimpleSchema({
+    email: { type: String, regEx: SimpleSchema.RegEx.Email },
+    firstName: { type: String },
+  }), optional: true },
 
   isPublished: { type: Boolean },
   isPrivate: { type: Boolean },
