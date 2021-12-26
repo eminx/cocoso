09f6fb5d1304fcc01d17ca43c31009c0d13b4345
ig <ilkergonenc@gmail.com>
2021-12-26 12:35:33 +0300

    reshape activity schema

--- imports/api/activities/activity.js
+++ imports/api/activities/activity.js
@@ -4,26 +4,6 @@ import { Schemas } from '../@/schemas';
 
 const Activities = new Mongo.Collection('activities');
 
-const SchemasActivity  = {
-  datesAndTimes: {
-    startDate: {type: String},
-    startTime: {type: String},
-    endDate: {type: String},
-    endTime: {type: String},
-    capacity: {type: SimpleSchema.Integer},
-    isRange: {type: Boolean},
-    conflict: {type: String, optional: true},
-
-    attendees: {type: Array, optional: true},
-    'attendees.$': {type: Object, optional: true},
-    'attendees.$.email': Schemas.Email,
-    'attendees.$.firstName': {type: String},
-    'attendees.$.lastName': {type: String},
-    'attendees.$.numberOfPeople': {type: SimpleSchema.Integer},
-    'attendees.$.registerDate': {type: Date},
-  }
-};
-
 Schemas.Activity = new SimpleSchema({
   _id: Schemas.Id,
   host: Schemas.Hostname,
@@ -47,7 +27,26 @@ Schemas.Activity = new SimpleSchema({
   room: {type: String, optional: true}, //undefined
 
   datesAndTimes: {type: Array},
-  'datesAndTimes.$': new SimpleSchema(SchemasActivity.datesAndTimes),
+  'datesAndTimes.$': new SimpleSchema({
+
+    startDate: {type: String},
+    startTime: {type: String},
+    endDate: {type: String},
+    endTime: {type: String},
+    capacity: {type: SimpleSchema.Integer},
+    isRange: {type: Boolean},
+    conflict: {type: String, optional: true},
+
+    attendees: {type: Array, optional: true},
+    'attendees.$': {type: new SimpleSchema({
+      email: Schemas.Email,
+      firstName: {type: String},
+      lastName: {type: String},
+      numberOfPeople: {type: SimpleSchema.Integer},
+      registerDate: {type: Date},
+    }), optional: true},
+    
+  }),
 
   practicalInfo: {type: String, optional: true}, // null
   internalInfo: {type: String, optional: true}, // null
