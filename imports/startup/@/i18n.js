de38cc2c91073d76c5b3ba3b77e2a6b8444ced20
ig <ilkergonenc@gmail.com>
2022-02-08 12:59:20 +0300

    create and add calendar locale files

--- imports/startup/@/i18n.js
+++ imports/startup/@/i18n.js
@@ -14,7 +14,7 @@ const options = {
   preload: [defaultLang],
   load: 'languageOnly', // we only provide en, de -> no region specific locals like en-US, de-DE
   // have a common namespace used around the full app
-  ns: ['common', 'accounts', 'members', 'hosts', 'admin', 'activities'],
+  ns: ['common', 'accounts', 'members', 'hosts', 'admin', 'activities', 'calendar'],
   defaultNS: 'common',
 
   // saveMissing: true,
