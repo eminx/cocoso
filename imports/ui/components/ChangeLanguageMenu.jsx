c36cddd7d7bf03620c1b5f9ad9b27048f880ffa6
ig <ilkergonenc@gmail.com>
2022-02-14 13:58:14 +0300

    add select box language menu

--- imports/ui/components/ChangeLanguageMenu.jsx
+++ imports/ui/components/ChangeLanguageMenu.jsx
@@ -5,24 +5,39 @@ import {
   MenuButton,
   MenuList,
   MenuItem,
+  Select
 } from '@chakra-ui/react';
 
-export default ChangeLanguage = () => {
+export default ChangeLanguage = ({ select, register }) => {
   const { t, i18n } = useTranslation();
+
+  if (!select)
+    return (
+      <Menu>
+        <MenuButton textTransform="uppercase">{i18n.language}</MenuButton>
+        <MenuList>
+          {i18n.languages.map(lang => 
+            <MenuItem 
+              key={lang}
+              fontWeight={lang == i18n.language ? 'bold' : 'normal'}
+              onClick={() => i18n.changeLanguage(lang)}
+            >
+              {t('common:langs.'+lang)}
+            </MenuItem>
+          )}
+        </MenuList>
+      </Menu>
+    );
+
   return (
-    <Menu>
-      <MenuButton textTransform="uppercase">{i18n.language}</MenuButton>
-      <MenuList>
-        {i18n.languages.map(lang => 
-          <MenuItem 
-            key={lang}
-            fontWeight={lang == i18n.language ? 'bold' : 'normal'}
-            onClick={() => i18n.changeLanguage(lang)}
-          >
-            {t('common:langs.'+lang)}
-          </MenuItem>
-        )}
-      </MenuList>
-    </Menu>
+    <Select
+      name="lang" 
+      {...register('lang')}
+      placeholder={t('common:langs.form.holder')}
+    >
+      {i18n.languages.map(lang => 
+        <option value={lang}>{t('common:langs.'+lang)}</option>
+      )}
+    </Select>
   );
 };
