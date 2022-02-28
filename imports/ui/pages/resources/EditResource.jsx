3d5ce05b5e2f56230c32b10b812965ad5ab585b2
ig <ilkergonenc@gmail.com>
2022-02-28 16:53:36 +0300

    set data tracker & content for single + edit resource pages

--- imports/ui/pages/resources/EditResource.jsx
+++ imports/ui/pages/resources/EditResource.jsx
@@ -1,24 +1,34 @@
+import { Meteor } from 'meteor/meteor';
 import { withTracker } from 'meteor/react-meteor-data';
 
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Box, Center } from '@chakra-ui/react';
+import { useTranslation } from 'react-i18next';
+
+import Resources from '/imports/api/resources/resource';
 
 import Template from '../../components/Template';
+import ResourceForm from '../../components/ResourceForm';
 
-function EditResourcePage({ resourceId }) {
-
+function EditResourcePage({ resource }) {
+  const [ tc ] = useTranslation('common');
   return (
-    <Template>
-      <h1>Edit Resource ({resourceId})</h1>
-      <Link to={`/resources/${resourceId}`}>Back to resource</Link>
+    <Template heading={tc('labels.update', { domain: tc('domains.resource') })}>
+      <Center>
+        <Link to={`/resources/${resource._id}`}>Back to resource</Link>
+      </Center>
+      <Box bg="white" p="6">
+        <ResourceForm />
+      </Box>
     </Template>
   );
 }
 
 export default EditResource = withTracker((props) => {
   const resourceId = props.match.params.id;
-
-  return {
-    resourceId
-  };
+  const handler = Meteor.subscribe('resources');
+  if (!handler.ready()) return { resource: {} };
+  const resource =  Resources.findOne({ _id: resourceId });
+  return { resource };
 })(EditResourcePage);
--- imports/ui/pages/resources/Resource.jsx
+++ imports/ui/pages/resources/Resource.jsx
@@ -1,24 +1,64 @@
+import { Meteor } from 'meteor/meteor';
 import { withTracker } from 'meteor/react-meteor-data';
 
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Box, Heading, Text, Button, Center } from '@chakra-ui/react';
+import { useTranslation } from 'react-i18next';
+
+import moment from 'moment';
+
+import Resources from '/imports/api/resources/resource';
 
 import Template from '../../components/Template';
+import ResourcesForCombo from '../../components/ResourcesForCombo';
 
-function ResourcePage({ resourceId }) {
+function ResourcePage({ resource }) {
+  const [ t ] = useTranslation('admin');
+  const [ tc ] = useTranslation('common');
 
   return (
     <Template>
-      <h1>Resource ({resourceId})</h1>
-      <Link to={`/resources/${resourceId}/edit`}>Edit resource</Link>
+      <Center>
+        <Link to={`/resources`}>Back to resources</Link>
+      </Center>
+      <Box bg="white" mb="2" p="2" key={resource.label}>
+        <Heading size="md" fontWeight="bold">
+          {resource.isCombo ? (
+            <ResourcesForCombo resource={resource} />
+          ) : (
+            resource.label
+          )}
+        </Heading>
+        <Text as="div" my="2">
+          {resource && resource.description}
+        </Text>
+        <Box py="2">
+          <Text as="div" fontSize="xs">
+            {t('resources.cards.date', { 
+              username: resource && resource.authorUsername, 
+              date: moment(resource.creationDate).format('D MMM YYYY')
+            })}
+            <br />
+          </Text>
+        </Box>
+      </Box>
+
+      <Center my="2">
+        <Link to={`/resources/${resource._id}/edit`}>
+          <Button size="sm" variant="ghost">
+            {tc('actions.update')}
+          </Button>
+        </Link>
+      </Center>
     </Template>
   );
 }
 
 export default Resource = withTracker((props) => {
   const resourceId = props.match.params.id;
-
-  return {
-    resourceId
-  };
+  const handler = Meteor.subscribe('resources');
+  if (!handler.ready()) return { resource: {}, isLoading: true };
+  const resource =  Resources.findOne({ _id: resourceId });
+  return { resource };
 })(ResourcePage);
