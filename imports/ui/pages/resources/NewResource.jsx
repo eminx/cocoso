5c16e0524ab8cd85ae25c44322a31ba1f58df186
ig <ilkergonenc@gmail.com>
2022-02-28 14:01:26 +0300

    create new-resource page

---
+++ imports/ui/pages/resources/NewResource.jsx
@@ -0,0 +1,14 @@
+
+import React from 'react';
+
+import Template from '../../components/Template';
+
+function NewResource() {
+  return (
+    <Template>
+      <h1>New Resource</h1>
+    </Template>
+  );
+}
+
+export default NewResource;
