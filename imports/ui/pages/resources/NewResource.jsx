ee2ce083b6cf05b68d92d67b9950cf93575395dd
ig <ilkergonenc@gmail.com>
2022-02-28 16:51:49 +0300

    set content for new resource page

--- imports/ui/pages/resources/NewResource.jsx
+++ imports/ui/pages/resources/NewResource.jsx
@@ -1,12 +1,22 @@
 
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Box, Center } from '@chakra-ui/react';
+import { useTranslation } from 'react-i18next';
 
 import Template from '../../components/Template';
+import ResourceForm from '../../components/ResourceForm';
 
 function NewResource() {
+  const [ tc ] = useTranslation('common');
   return (
-    <Template>
-      <h1>New Resource</h1>
+    <Template heading={tc('labels.create', { domain: tc('domains.resource') })}>
+      <Center>
+        <Link to={`/resources`}>Back to resources</Link>
+      </Center>
+      <Box bg="white" p="6">
+        <ResourceForm />
+      </Box>
     </Template>
   );
 }
