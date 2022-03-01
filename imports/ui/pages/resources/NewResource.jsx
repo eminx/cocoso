f4246bc76eb85e67e5a9fd06cfbbe8758f5f22ff
ig <ilkergonenc@gmail.com>
2022-03-01 18:25:07 +0300

    set new resource page working

--- imports/ui/pages/resources/NewResource.jsx
+++ imports/ui/pages/resources/NewResource.jsx
@@ -1,24 +1,49 @@
+import { Meteor } from 'meteor/meteor';
+import { withTracker } from 'meteor/react-meteor-data';
 
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Box, Center } from '@chakra-ui/react';
 import { useTranslation } from 'react-i18next';
 
+import Resources from '/imports/api/resources/resource';
+
 import Template from '../../components/Template';
 import ResourceForm from './components/ResourceForm';
 
-function NewResource() {
+function NewResourcePage({ resources, isLoading }) {
   const [ tc ] = useTranslation('common');
+  
+  const resourceModel = {
+    _id: '',
+    label: '',
+    description: '',
+    isCombo: false,
+    resourcesForCombo: [],
+  };
+
   return (
     <Template heading={tc('labels.create', { domain: tc('domains.resource') })}>
       <Center>
         <Link to={`/resources`}>Back to resources</Link>
       </Center>
       <Box bg="white" p="6">
-        <ResourceForm />
+        {!isLoading 
+          && <ResourceForm 
+              resources={resources}
+              defaultValues={resourceModel}
+              isEditMode={false}
+              comboResources={resourceModel.resourcesForCombo} 
+            />
+        }
       </Box>
     </Template>
   );
 }
 
-export default NewResource;
+export default NewResource = withTracker(() => {
+  const handler = Meteor.subscribe('resources');
+  if (!handler.ready()) return { resources: [], isLoading: true };
+  const resources =  Resources.find({ }, { fields: { label: 1 }}).fetch().reverse();
+  return { resources, isLoading: false };
+})(NewResourcePage);
