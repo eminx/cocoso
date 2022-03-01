8eaba2075440e58650b9826a90882e694e89f1a6
ig <ilkergonenc@gmail.com>
2022-03-01 18:25:21 +0300

    set edit page working

--- imports/ui/pages/resources/EditResource.jsx
+++ imports/ui/pages/resources/EditResource.jsx
@@ -1,26 +1,74 @@
 import { Meteor } from 'meteor/meteor';
 import { withTracker } from 'meteor/react-meteor-data';
 
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { Box, Center } from '@chakra-ui/react';
+import { Box, Center, Button } from '@chakra-ui/react';
 import { useTranslation } from 'react-i18next';
 
 import Resources from '/imports/api/resources/resource';
 
+import { call } from '../../@/shared';
+import { message } from '../../components/message';
+import ConfirmModal from '../../components/ConfirmModal';
+
 import Template from '../../components/Template';
 import ResourceForm from './components/ResourceForm';
 
-function EditResourcePage({ resource }) {
+function EditResourcePage({ resources, resource, resourcesForCombo, isLoading }) {
   const [ tc ] = useTranslation('common');
+  const [ isDeleteModalOn, setIsDeleteModalOn ] = useState(false)
+  
+  const hideDeleteModal = () => setIsDeleteModalOn(false);
+  const showDeleteModal = () => setIsDeleteModalOn(true);
+
+  const deleteResource = async (resourceId) => {
+    try {
+      await call('deleteResource', resourceId);
+      message.success(tc('message.success.remove', { domain: tc('domains.resource') }));
+    } catch (error) {
+      message.error(error.error || error.reason);
+      console.log(error);
+    }
+  };
+
   return (
     <Template heading={tc('labels.update', { domain: tc('domains.resource') })}>
       <Center>
         <Link to={`/resources/${resource._id}`}>Back to resource</Link>
       </Center>
       <Box bg="white" p="6">
-        <ResourceForm />
+        {!isLoading 
+          && <ResourceForm 
+              resources={resources}
+              defaultValues={resource} 
+              isEditMode={true} 
+              comboResources={resourcesForCombo} 
+            />
+        }
+        
       </Box>
+
+      <Center p="4">
+        <Button
+          colorScheme="red"
+          size="sm"
+          variant="ghost"
+          onClick={showDeleteModal}
+        >
+          {tc('actions.remove')}
+        </Button>
+      </Center>
+
+      <ConfirmModal
+        visible={isDeleteModalOn}
+        title={tc('modals.confirm.delete.title')}
+        onConfirm={() => deleteResource(resource._id)}
+        onCancel={hideDeleteModal}
+        confirmText={tc('modals.confirm.delete.yes')}
+      >
+        {tc('modals.confirm.delete.body', { domain: tc('domains.resource').toLowerCase() })}
+      </ConfirmModal>
     </Template>
   );
 }
@@ -28,7 +76,21 @@ function EditResourcePage({ resource }) {
 export default EditResource = withTracker((props) => {
   const resourceId = props.match.params.id;
   const handler = Meteor.subscribe('resources');
-  if (!handler.ready()) return { resource: {} };
-  const resource =  Resources.findOne({ _id: resourceId });
-  return { resource };
+  if (!handler.ready()) return { resources: [], resource: {}, resourcesForCombo: [], isLoading: true };
+  const resources =  Resources.find({}).fetch().reverse();
+  const resource =  Resources.findOne(
+    { _id: resourceId }, 
+    { 
+      fields: { 
+        label: 1,
+        description: 1,
+        isCombo: 1,
+        resourcesForCombo: 1
+      },
+    }
+  );  
+  const resourcesForCombo = resource?.resourcesForCombo;
+  return { resources, resource, resourcesForCombo, isLoading: false };
 })(EditResourcePage);
+
+
