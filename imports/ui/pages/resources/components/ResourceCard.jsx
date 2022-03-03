fcc4e244ab6916a14050b6ed3af9212f3d1a0086
ig <ilkergonenc@gmail.com>
2022-03-03 17:13:41 +0300

    create Resource Card and move ForCombo init

---
+++ imports/ui/pages/resources/components/ResourceCard.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import { Box, Heading, Flex, Tag, Text } from '@chakra-ui/react';
+import { useTranslation } from 'react-i18next';
+import moment from 'moment';
+
+export default function ResourcesCard({ resource }) {
+  const [ t ] = useTranslation('admin');
+  return (
+    <Box bg="white" mb="2" p="2" key={resource?.label}>
+      <Heading size="md" fontWeight="bold">
+        {resource?.isCombo ? (
+          <ResourcesForCombo resource={resource} />
+        ) : (
+          resource?.label
+        )}
+      </Heading>
+      <Text as="div" my="2">
+        {resource && resource?.description}
+      </Text>
+      <Box py="2">
+        <Text as="div" fontSize="xs">
+          {t('resources.cards.date', { 
+            username: resource && resource?.authorUsername, 
+            date: moment(resource?.creationDate).format('D MMM YYYY')
+          })}
+          <br />
+        </Text>
+      </Box>
+    </Box>
+  );
+}
+
+function ResourcesForCombo({ resource }) {
+  const [ t ] = useTranslation('admin');
+  const resourcesForCombo = resource?.resourcesForCombo;
+  const length = resource?.resourcesForCombo.length;
+
+  return (
+    <span>
+      <Flex mb="2">
+        <Text mr="2">{resource?.label}</Text>
+        <Tag size="sm" textTransform="uppercase">{t('resources.cards.ifCombo')}</Tag>
+      </Flex>
+      {' ['}
+      {resourcesForCombo.map((res, i) => (
+        <Text as="span" fontSize="sm" key={res._id}>
+          {res.label + (i < length - 1 ? ' + ' : '')}
+        </Text>
+      ))}
+      ]
+    </span>
+  );
+}
