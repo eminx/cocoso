73c9fce2ca4fb98dd09b8465b7546d8e8356fdf9
ig <ilkergonenc@gmail.com>
2022-03-03 18:34:12 +0300

    created breadcrumb component on new routes via window.location

---
+++ imports/ui/components/Breadcrumb.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { 
+  Breadcrumb as BreadcrumbMenu,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  Center,
+} from '@chakra-ui/react';
+
+export default function Breadcrumb({ domain, domainKey }) {
+  const [ breadcrumbs ] = useState(window.location.pathname.split('/'));
+  return (
+    <Center py="4">
+      <BreadcrumbMenu>
+        {breadcrumbs.map((item, index) => {
+          if(domain?._id == item) item = domain[domainKey];
+          item = item.charAt(0).toUpperCase() + item.slice(1);
+          if(item=='' && index==0) return (
+            <BreadcrumbItem>
+              <Link to='/'>Home</Link>
+            </BreadcrumbItem>
+          );
+          if (index!=breadcrumbs.length-1) {
+            let href = '';
+            for (let i = 0; i < index+1; i++) {
+              if (breadcrumbs[i]!='') href = href + '/' + breadcrumbs[i]
+            }
+            return (
+              <BreadcrumbItem>
+                <Link to={href}>{item}</Link>
+              </BreadcrumbItem>
+            );
+          }
+          if (index==breadcrumbs.length-1) {
+            return (
+              <BreadcrumbItem isCurrentPage>
+                <BreadcrumbLink href="#">{item}</BreadcrumbLink>
+              </BreadcrumbItem>
+            );
+          }
+        })}
+      </BreadcrumbMenu>
+    </Center>
+  );
+};
