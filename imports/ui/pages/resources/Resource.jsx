a31cedfee024ed7f984ad06fc8c6e4f7f88820ed
ig <ilkergonenc@gmail.com>
2022-03-03 17:17:33 +0300

    edit Resource page with  Card

--- imports/ui/pages/resources/Resource.jsx
+++ imports/ui/pages/resources/Resource.jsx
@@ -3,19 +3,16 @@ import { withTracker } from 'meteor/react-meteor-data';
 
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Box, Heading, Text, Button, Center } from '@chakra-ui/react';
+import { Button, Center } from '@chakra-ui/react';
 import { useTranslation } from 'react-i18next';
-
-import moment from 'moment';
 
 import Resources from '/imports/api/resources/resource';
 
 import NotFoundPage from '../NotFoundPage';
 import Template from '../../components/Template';
-import ResourcesForCombo from './components/ResourcesForCombo';
+import ResourceCard from './components/ResourceCard';
 
 function ResourcePage({ resource }) {
-  const [ t ] = useTranslation('admin');
   const [ tc ] = useTranslation('common');
 
   if (typeof resource === 'undefined')  return <NotFoundPage domain="Resource with this name or id" />;
@@ -25,27 +22,7 @@ function ResourcePage({ resource }) {
       <Center>
         <Link to={`/resources`}>Back to resources</Link>
       </Center>
-      <Box bg="white" mb="2" p="2" key={resource?.label}>
-        <Heading size="md" fontWeight="bold">
-          {resource?.isCombo ? (
-            <ResourcesForCombo resource={resource} />
-          ) : (
-            resource?.label
-          )}
-        </Heading>
-        <Text as="div" my="2">
-          {resource && resource?.description}
-        </Text>
-        <Box py="2">
-          <Text as="div" fontSize="xs">
-            {t('resources.cards.date', { 
-              username: resource && resource?.authorUsername, 
-              date: moment(resource?.creationDate).format('D MMM YYYY')
-            })}
-            <br />
-          </Text>
-        </Box>
-      </Box>
+      <ResourceCard resource={resource}/>
       <Center my="2">
         <Link to={`/resources/${resource?._id}/edit`}>
           <Button size="sm" variant="ghost">
