e8bcb5b090de32d7bc69cbdc2fbcece7b51707a7
ig <ilkergonenc@gmail.com>
2022-03-12 19:10:19 +0300

    clean schema and activate back

--- imports/api/resources/resource.js
+++ imports/api/resources/resource.js
@@ -10,45 +10,18 @@ Resources.schema = new SimpleSchema({
   
   authorId: Schemas.Id,
   authorUsername: {type: String},
-  // authorFirstName: {type: String},
-  // authorLastName: {type: String},
-  // authorAvatar: {type: new SimpleSchema(Schemas.Avatar), optional: true },
 
   label: {type: String},
-  // labelLowerCase: {type: String},
   description: {type: String},
-  // hourlyFee: {type: String, optional: true},
   isCombo: {type: Boolean},
 
   resourceIndex: {type: SimpleSchema.Integer},
   resourcesForCombo: {type: Array},
   'resourcesForCombo.$': Schemas.Id,
-  // 'resourcesForCombo.$': new SimpleSchema({
-  //   _id: Schemas.Id,
-  //   // host: {type: new SimpleSchema(Schemas.Hostname), optional: true },
-    
-  //   // authorId: {type: new SimpleSchema(Schemas.Id), optional: true },
-  //   // authorUsername: {type: String, optional: true },
-  //   // authorFirstName: {type: String, optional: true },
-  //   // authorLastName: {type: String, optional: true },
-  //   // authorAvatar: {type: new SimpleSchema(Schemas.Avatar), optional: true },
-
-  //   label: {type: String},
-  //   // labelLowerCase: {type: String, optional: true },
-  //   // description: {type: String, optional: true },
-  //   // hourlyFee: {type: String, optional: true},
-  //   // isCombo: {type: Boolean, optional: true },
-  //   // resourceIndex: {type: SimpleSchema.Integer, optional: true},
-    
-  //   // updatedBy: {type: String, optional: true},
-  //   // latestUpdate: {type: Date, optional: true},
-  //   // creationDate: {type: Date, optional: true },
-  // }),
 
   createdAt: {type: Date},
   updatedAt: {type: Date, optional: true},
   updatedBy: {type: String, optional: true},
-
 });
 
 Resources.attachSchema(Resources.schema);
