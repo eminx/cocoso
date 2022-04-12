245a59b518329a0013ed7945ddc5ff615f52b935
Emin Durak <emin@tuta.io>
2022-04-12 13:01:23 +0300

    uses try over meteor call

--- imports/ui/pages/resources/components/BookingsField.jsx
+++ imports/ui/pages/resources/components/BookingsField.jsx
@@ -6,9 +6,24 @@ import moment from 'moment';
 moment.locale(i18n.language);
 
 import { useForm } from 'react-hook-form';
-import { Box, Flex, Heading, Text, Button, HStack, Textarea, 
-  Switch, FormControl, FormLabel, Code,
-  Accordion, AccordionItem, AccordionButton, AccordionPanel, Input, } from '@chakra-ui/react';
+import {
+  Box,
+  Flex,
+  Heading,
+  Text,
+  Button,
+  HStack,
+  Textarea,
+  Switch,
+  FormControl,
+  FormLabel,
+  Code,
+  Accordion,
+  AccordionItem,
+  AccordionButton,
+  AccordionPanel,
+  Input,
+} from '@chakra-ui/react';
 import { AddIcon, MinusIcon } from '@chakra-ui/icons';
 
 import { call } from '../../../@/shared';
@@ -20,37 +35,40 @@ import { StateContext } from '../../../LayoutContainer';
 const today = new Date().toISOString().substring(0, 10);
 
 export default function BookingsField({ domain }) {
-
   const { role, canCreateContent } = useContext(StateContext);
   const isAdmin = role === 'admin' ? true : false;
 
-  const [ bookings, setBookings] = useState([]);
-  const [ newBooking, setNewBooking ] = useState({
-    startDate: today, 
+  const [bookings, setBookings] = useState([]);
+  const [newBooking, setNewBooking] = useState({
+    startDate: today,
     endDate: today,
-    startTime: '', 
-    endTime: ''
+    startTime: '',
+    endTime: '',
   });
-  const [ multipledays, setMultipledays ] = useState(false);
-  const [ occurences, setOccurences] = useState([]);
-  const [ isLoading, setIsLoading ] = useState(true);
+  const [multipledays, setMultipledays] = useState(false);
+  const [occurences, setOccurences] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   const { formState, handleSubmit, register } = useForm();
   const { isDirty, isSubmitting } = formState;
 
-  const [ t ] = useTranslation('resources');
-  const [ tc ] = useTranslation('common');
-  
+  const [t] = useTranslation('resources');
+  const [tc] = useTranslation('common');
 
   const getBookings = async () => {
     try {
       const response = await call('getResourceBookingsForUser', domain?._id);
       setBookings(
-        response.map(booking =>  ({ ...booking, actions: [{
-          content: tc('labels.remove'),
-          handleClick: () => removeBooking(booking._id),
-        }] 
-      })));
+        response.map((booking) => ({
+          ...booking,
+          actions: [
+            {
+              content: tc('labels.remove'),
+              handleClick: () => removeBooking(booking._id),
+            },
+          ],
+        }))
+      );
       setIsLoading(false);
     } catch (error) {
       message.error(error.reason);
@@ -66,7 +84,7 @@ export default function BookingsField({ domain }) {
     setIsLoading(true);
     try {
       const response = await call('getAllOccurences');
-      setOccurences([ ...response ]);
+      setOccurences([...response]);
       setIsLoading(false);
     } catch (error) {
       message.error(error.reason);
@@ -77,7 +95,7 @@ export default function BookingsField({ domain }) {
   useEffect(() => {
     getBookings();
   }, [bookings]);
-  
+
   const removeBooking = async (bookingId) => {
     if (!isAdmin) {
       message.error(tc('message.access.deny'));
@@ -89,25 +107,22 @@ export default function BookingsField({ domain }) {
       message.error(error.reason);
     }
   };
-    
+
   const handleDateAndTimeChange = (value, key) => {
     newBooking[key] = value;
-    setNewBooking({...newBooking});
+    setNewBooking({ ...newBooking });
     validateBookings(newBooking, domain);
   };
 
   const validateBookings = (selectedOccurence, selectedResource) => {
-
-    const allOccurencesWithSelectedResource = occurences.filter(
-      (occurence) => {
-        if (selectedResource.isCombo) {
-          return selectedResource.resourcesForCombo.some((resourceForCombo) => {
-            return resourceForCombo.label === occurence.resource;
-          });
-        }
-        return occurence.resource === selectedResource.label;
+    const allOccurencesWithSelectedResource = occurences.filter((occurence) => {
+      if (selectedResource.isCombo) {
+        return selectedResource.resourcesForCombo.some((resourceForCombo) => {
+          return resourceForCombo.label === occurence.resource;
+        });
       }
-    );
+      return occurence.resource === selectedResource.label;
+    });
 
     // console.log('allOccurencesWithSelectedResource: ', allOccurencesWithSelectedResource);
 
@@ -131,10 +146,8 @@ export default function BookingsField({ domain }) {
       }
     );
 
-    // console.log('occurenceWithConflict: ', occurenceWithConflict);
-
     if (occurenceWithConflict) {
-      if (selectedOccurence?.conflict) delete selectedOccurence.conflict
+      if (selectedOccurence?.conflict) delete selectedOccurence.conflict;
       setNewBooking({
         ...selectedOccurence,
         conflict: {
@@ -142,17 +155,15 @@ export default function BookingsField({ domain }) {
         },
       });
     } else {
-      delete selectedOccurence.conflict
+      delete selectedOccurence.conflict;
       setNewBooking(selectedOccurence);
     }
-
-    // console.log(newBooking);
   };
 
   const onSubmit = async (values) => {
     multipledays === false
-      ? values = { ...newBooking, endDate: newBooking.startDate, ...values }
-      : values = { ...newBooking, ...values };
+      ? (values = { ...newBooking, endDate: newBooking.startDate, ...values })
+      : (values = { ...newBooking, ...values });
 
     activityValues = {
       title: values.title,
@@ -168,24 +179,31 @@ export default function BookingsField({ domain }) {
       internalInfo: '',
       address: '',
       capacity: 0,
-      datesAndTimes: [{
-        startDate: values.startDate,
-        startTime: values.startTime,
-        endDate: values.endDate,
-        endTime: values.endTime,
-        capacity: 0,
-      }],
+      datesAndTimes: [
+        {
+          startDate: values.startDate,
+          startTime: values.startTime,
+          endDate: values.endDate,
+          endTime: values.endTime,
+          capacity: 0,
+        },
+      ],
       isPublicActivity: false,
       isRegistrationDisabled: true,
     };
 
-    Meteor.call('createActivity', activityValues, '', (error, result) => {
-      if (error) {
-        console.log('error', error);
-        message.error(error.reason);
-      }
-    });
-
+    try {
+      call('createActivity', activityValues, '');
+      message.success(
+        tc('message.success.create', {
+          domain: `${tc('domains.your')} ${tc(
+            'domains.activity'
+          ).toLowerCase()}`,
+        })
+      );
+    } catch (error) {
+      message.error(error.reason);
+    }
   };
 
   return (
@@ -201,59 +219,92 @@ export default function BookingsField({ domain }) {
               <>
                 <h2>
                   <AccordionButton>
-                    <Box flex='1' textAlign='left'>
+                    <Box flex="1" textAlign="left">
                       {t('booking.labels.form')}
                     </Box>
                     {isExpanded ? (
-                      <MinusIcon fontSize='12px' />
+                      <MinusIcon fontSize="12px" />
                     ) : (
-                      <AddIcon fontSize='12px' />
+                      <AddIcon fontSize="12px" />
                     )}
                   </AccordionButton>
                 </h2>
                 <AccordionPanel pb={4}>
-                  {canCreateContent &&       
+                  {canCreateContent && (
                     <form onSubmit={handleSubmit((data) => onSubmit(data))}>
-                      <FormControl display='flex' alignItems='center' mb="2">
-                        <Switch id='book-multiple-days' size="sm" onChange={() => setMultipledays(!multipledays) } />
-                        <FormLabel htmlFor='book-multiple-days' fontSize="sm" mb='0' ml="2">
+                      <FormControl display="flex" alignItems="center" mb="2">
+                        <Switch
+                          id="book-multiple-days"
+                          size="sm"
+                          onChange={() => setMultipledays(!multipledays)}
+                        />
+                        <FormLabel
+                          htmlFor="book-multiple-days"
+                          fontSize="sm"
+                          mb="0"
+                          ml="2"
+                        >
                           {t('booking.multiple')}
                         </FormLabel>
                       </FormControl>
 
-                      <Box p={newBooking.conflict ? '2' : '0'} mb="4" border={newBooking.conflict ? '1px solid red' : 'none'}>
+                      <Box
+                        p={newBooking.conflict ? '2' : '0'}
+                        mb="4"
+                        border={newBooking.conflict ? '1px solid red' : 'none'}
+                      >
                         <HStack spacing="2" mb="4">
-                          <DatePicker 
-                            noTime 
+                          <DatePicker
+                            noTime
                             placeholder={t('booking.date.start')}
-                            onChange={(date) => handleDateAndTimeChange(date, 'startDate')} 
+                            onChange={(date) =>
+                              handleDateAndTimeChange(date, 'startDate')
+                            }
                           />
-                          {multipledays && 
-                          <DatePicker 
-                            noTime 
-                            placeholder={t('booking.date.start')}
-                            onChange={(date) => handleDateAndTimeChange(date, 'endDate')} 
-                          />}
+                          {multipledays && (
+                            <DatePicker
+                              noTime
+                              placeholder={t('booking.date.start')}
+                              onChange={(date) =>
+                                handleDateAndTimeChange(date, 'endDate')
+                              }
+                            />
+                          )}
                         </HStack>
                         <HStack spacing="2">
                           <DatePicker
                             onlyTime
                             placeholder={t('booking.time.start')}
-                            onChange={(time) => handleDateAndTimeChange(time, 'startTime')}
+                            onChange={(time) =>
+                              handleDateAndTimeChange(time, 'startTime')
+                            }
                           />
                           <DatePicker
                             onlyTime
                             placeholder={t('booking.time.end')}
-                            onChange={(time) => handleDateAndTimeChange(time, 'endTime')}
+                            onChange={(time) =>
+                              handleDateAndTimeChange(time, 'endTime')
+                            }
                           />
                         </HStack>
                         {newBooking.conflict && (
                           <Box mt="4">
-                            <Text fontSize="sm" textAlign="center" fontWeight="bold">
+                            <Text
+                              fontSize="sm"
+                              textAlign="center"
+                              fontWeight="bold"
+                            >
                               {t('booking.conflict')}
                             </Text>
-                            <Code colorScheme='red' mx="auto" display="block" width="fit-content" mt="4">
-                              {newBooking.conflict.startDate === newBooking.conflict.endDate
+                            <Code
+                              colorScheme="red"
+                              mx="auto"
+                              display="block"
+                              width="fit-content"
+                              mt="4"
+                            >
+                              {newBooking.conflict.startDate ===
+                              newBooking.conflict.endDate
                                 ? newBooking.conflict.startDate
                                 : newBooking.conflict.startDate +
                                   '-' +
@@ -293,7 +344,7 @@ export default function BookingsField({ domain }) {
                         </Button>
                       </Flex>
                     </form>
-                  }
+                  )}
                 </AccordionPanel>
               </>
             )}
@@ -301,20 +352,21 @@ export default function BookingsField({ domain }) {
         </Accordion>
       </Box>
 
-      {!isLoading && 
-        <Box
-          bg="white" mt="2"
-        >
+      {!isLoading && (
+        <Box bg="white" mt="2">
           {bookings && bookings.length > 0 ? (
-            <NiceList
-              actionsDisabled={!isAdmin}
-              list={bookings} 
-            >
+            <NiceList actionsDisabled={!isAdmin} list={bookings}>
               {(booking) => (
                 <Box>
                   <Text fontSize="sm">
-                    {`From ${moment(booking.startDate).format('ddd, D MMM')} ${booking.startTime} 
-                    to ${booking.startDate === booking.endDate ? '' : moment(booking.endDate).format('ddd, D MMM')} ${booking.endTime} `}
+                    {`From ${moment(booking.startDate).format('ddd, D MMM')} ${
+                      booking.startTime
+                    } 
+                    to ${
+                      booking.startDate === booking.endDate
+                        ? ''
+                        : moment(booking.endDate).format('ddd, D MMM')
+                    } ${booking.endTime} `}
                   </Text>
                 </Box>
               )}
@@ -325,7 +377,7 @@ export default function BookingsField({ domain }) {
             </Text>
           )}
         </Box>
-      }
+      )}
     </Box>
   );
-};
+}
