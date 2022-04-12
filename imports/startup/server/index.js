1c5c0dea3bf962f6fd8d2e18a2f39b7f0d73d17b
Emin Durak <emin@tuta.io>
2022-04-12 12:54:36 +0300

    removes dangerous run of startup db document creation

--- imports/startup/server/index.js
+++ imports/startup/server/index.js
@@ -26,20 +26,6 @@ Meteor.startup(() => {
     url = url.replace('#/', '');
     return `To reset your password, simply click the link below. ${url}`;
   };
-
-  Resources.find().forEach((r) => {
-    Chats.insert({
-      host: r.host,
-      contextId: r._id,
-      contextName: r.label,
-      createdBy: {
-        userId: 'P7gktqsquKYsDZAxe',
-        username: 'emin',
-      },
-      isNotificationOn: false,
-      messages: new Array(),
-    });
-  });
 });
 
 import './migrations';
