096840cfa714d0654834fb42b476e06cefb065ac
Emin Durak <emin@tuta.io>
2022-04-13 21:09:59 +0300

    left content gets up on mobile

--- imports/ui/components/Template.jsx
+++ imports/ui/components/Template.jsx
@@ -19,50 +19,49 @@ function Template({
   children,
 }) {
   return (
-
-    <ScreenClassRender render={(screenClass) => {
-
-      const isMobile = ['xs', 'sm', 'md'].includes(screenClass);
-      return (
-        <Container fluid style={{ width: '100%', padding: 0, marginBottom: 24 }}>
-          <Helmet>
-            <title>{heading || publicSettings.name}</title>
-          </Helmet>
-          <Row gutterWidth={12} style={{ marginLeft: 0, marginRight: 0 }}>
-            {!isMobile && 
+    <ScreenClassRender
+      render={(screenClass) => {
+        const isMobile = ['xs', 'sm', 'md'].includes(screenClass);
+        return (
+          <Container
+            fluid
+            style={{ width: '100%', padding: 0, marginBottom: 24 }}
+          >
+            <Helmet>
+              <title>{heading || publicSettings.name}</title>
+            </Helmet>
+            <Row gutterWidth={12} style={{ marginLeft: 0, marginRight: 0 }}>
               <Col lg={3} style={colStyle}>
                 {leftContent}
               </Col>
-            }
-            <Col lg={6} style={{ ...colStyle, paddingLeft: 0, paddingRight: 0 }}>
-              {heading && (
-                <Box pl={titleCentered ? '0' : '2'} mb="2">
-                  <Heading
-                    as="h3"
-                    mb="4"
-                    size="md"
-                    textAlign={titleCentered ? 'center' : 'start'}
-                  >
-                    {heading}
-                  </Heading>
-                </Box>
-              )}
-              {children}
-            </Col>
-            {isMobile && 
-              <Col lg={3} style={colStyle}>
-                {leftContent}
+
+              <Col
+                lg={6}
+                style={{ ...colStyle, paddingLeft: 0, paddingRight: 0 }}
+              >
+                {heading && (
+                  <Box pl={titleCentered ? '0' : '2'} mb="2">
+                    <Heading
+                      as="h3"
+                      mb="4"
+                      size="md"
+                      textAlign={titleCentered ? 'center' : 'start'}
+                    >
+                      {heading}
+                    </Heading>
+                  </Box>
+                )}
+                {children}
               </Col>
-            }
-            <Col lg={3} style={colStyle}>
-              {rightContent}
-            </Col>
-          </Row>
-        </Container>
-      );
 
-    }} />
-
+              <Col lg={3} style={colStyle}>
+                {rightContent}
+              </Col>
+            </Row>
+          </Container>
+        );
+      }}
+    />
   );
 }
 
