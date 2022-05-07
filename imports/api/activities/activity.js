776919f5df60960639a5aab5a4c22694eeb58866
ig <ilkergonenc@gmail.com>
2022-05-07 16:42:37 +0300

    set some keys optional for temporary

--- imports/api/activities/activity.js
+++ imports/api/activities/activity.js
@@ -20,9 +20,9 @@ Activities.schema = new SimpleSchema({
   longDescription: { type: String, optional: true },
   imageUrl: { type: String, optional: true },
 
-  resourceId: { type: String, regEx: SimpleSchema.RegEx.Id },
+  resourceId: { type: String, regEx: SimpleSchema.RegEx.Id, optional: true },
   resource: { type: String, optional: true },
-  resourceIndex: { type: SimpleSchema.Integer },
+  resourceIndex: { type: SimpleSchema.Integer, optional: true },
   // resourceHourlyFee: {type: String, optional: true}, //undefined
 
   address: { type: String, optional: true },
