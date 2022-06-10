f58c8a58419cac952b836da2922a235239fe21c5
ig <ilkergonenc@gmail.com>
2022-06-10 14:34:44 +0300

    bug fix missing process content unchecked empty array

--- imports/ui/pages/processes/ProcessesList.jsx
+++ imports/ui/pages/processes/ProcessesList.jsx
@@ -141,18 +141,20 @@ export default function ProcessesList({ isLoading, currentUser, processes, t, tc
             <TabPanels>
               {filterOptions.map((option) => (
                 <TabPanel key={option.value}>
-                  <Paginate
-                    items={processesRendered}
-                    grid={{ columns: [1, 1, 2, 3], spacing: 3, w: '100%' }}
-                  >
-                    {(process) => (
-                      <Link key={process._id} to={`/process/${process._id}`}>
-                        <GridThumb image={process.imageUrl} large title={process.title}>
-                          {moment(process.creationDate).format('D MMM YYYY')}
-                        </GridThumb>
-                      </Link>
-                    )}
-                  </Paginate>
+                  {processesRendered.length > 0 && (
+                    <Paginate
+                      items={processesRendered}
+                      grid={{ columns: [1, 1, 2, 2], spacing: 3, w: '100%' }}
+                    >
+                      {(process) => (
+                        <Link key={process._id} to={`/process/${process._id}`}>
+                          <GridThumb image={process.imageUrl} large title={process.title}>
+                            {moment(process.creationDate).format('D MMM YYYY')}
+                          </GridThumb>
+                        </Link>
+                      )}
+                    </Paginate>
+                  )}
                 </TabPanel>
               ))}
             </TabPanels>
