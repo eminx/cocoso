614abd3595b883ddf33bdefdcbcecc72eb8851fd
Emin Durak <emin@tuta.io>
2022-06-12 18:40:32 +0300

    styling upgrades

--- imports/ui/components/ChangeLanguageMenu.jsx
+++ imports/ui/components/ChangeLanguageMenu.jsx
@@ -1,35 +1,42 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
-import { Menu, MenuButton, MenuList, MenuItem, Select } from '@chakra-ui/react';
+import { Box, Menu, MenuButton, MenuList, MenuItem, Select } from '@chakra-ui/react';
 
 export default ChangeLanguage = ({ select, register }) => {
   const { t, i18n } = useTranslation();
 
   if (!select)
     return (
-      <Menu>
-        <MenuButton textTransform="uppercase">{i18n.language}</MenuButton>
-        <MenuList>
-          {i18n.languages.map((lang) => (
-            <MenuItem
-              key={lang}
-              fontWeight={lang == i18n.language ? 'bold' : 'normal'}
-              onClick={() => i18n.changeLanguage(lang)}
-            >
-              {t(`common:langs.${lang}`)}
-            </MenuItem>
-          ))}
-        </MenuList>
-      </Menu>
+      <Box p="4">
+        <Menu>
+          <MenuButton textTransform="uppercase" fontWeight="bold">
+            {i18n.language}
+          </MenuButton>
+          <MenuList>
+            {i18n.languages.map((lang) => (
+              <MenuItem
+                key={lang}
+                color="gray.700"
+                fontWeight={lang == i18n.language ? 'bold' : 'normal'}
+                onClick={() => i18n.changeLanguage(lang)}
+              >
+                {t(`common:langs.${lang}`)}
+              </MenuItem>
+            ))}
+          </MenuList>
+        </Menu>
+      </Box>
     );
 
   return (
-    <Select name="lang" {...register('lang')} placeholder={t('common:langs.form.holder')}>
-      {i18n.languages.map((lang) => (
-        <option key={lang} value={lang}>
-          {t(`common:langs.${lang}`)}
-        </option>
-      ))}
-    </Select>
+    <Box p="4">
+      <Select name="lang" {...register('lang')} placeholder={t('common:langs.form.holder')}>
+        {i18n.languages.map((lang) => (
+          <option key={lang} value={lang}>
+            {t(`common:langs.${lang}`)}
+          </option>
+        ))}
+      </Select>
+    </Box>
   );
 };
