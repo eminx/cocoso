7212ba2e2fe59f2fa55999663ae0278f52a957d4
Emin Durak <emin@tuta.io>
2022-06-13 20:07:55 +0300

    removes users who deleted their profile

--- imports/startup/server/migrations.js
+++ imports/startup/server/migrations.js
@@ -459,7 +459,10 @@ Migrations.add({
       const members = host.members;
       const newMembers = members.map((m) => {
         const member = Meteor.users.findOne({ _id: m.id });
-        const avatar = member && member.avatar ? member.avatar.src : null;
+        if (!member) {
+          return;
+        }
+        const avatar = member.avatar ? member.avatar.src : null;
         return {
           ...m,
           avatar: avatar,
