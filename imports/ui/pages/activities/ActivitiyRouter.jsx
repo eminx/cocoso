ed5a8eb9a5b1390c649542c243dd04044735d134
ig <ilkergonenc@gmail.com>
2022-06-20 12:43:47 +0300

    create new routes for activities

---
+++ imports/ui/pages/activities/ActivitiyRouter.jsx
@@ -0,0 +1,20 @@
+import React, { lazy } from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+const Activities = lazy(() => import('./ActivitiesContainer'));
+const NewActivity = lazy(() => import('./NewActivityContainer'));
+const Activity = lazy(() => import('./ActivityContainer'));
+const EditActivity = lazy(() => import('./EditActivityContainer'));
+
+export default function ActivityRoutes({ path, history }) {
+  return (
+    <Switch path={path} history={history}>
+      <Route exact path="/activities" component={Activities} />
+      <Route exact path="/activities/new" component={NewActivity} history={history} />
+      <Switch>
+        <Route path="/activities/:activityId/edit" component={EditActivity} history={history} />
+        <Route path="/activities/:activityId" component={Activity} />
+      </Switch>
+    </Switch>
+  );
+}
