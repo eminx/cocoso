971f6b61e51c907eac99dc61cee9f4d5d9903eb7
ig <ilkergonenc@gmail.com>
2022-06-23 14:01:42 +0300

    add pages with a redirect to page-routes

--- imports/ui/pages/pages/PageRouter.jsx
+++ imports/ui/pages/pages/PageRouter.jsx
@@ -1,5 +1,5 @@
 import React, { lazy } from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 
 const NewPage = lazy(() => import('./NewPageContainer'));
 const Page = lazy(() => import('./Page'));
@@ -8,6 +8,13 @@ const EditPage = lazy(() => import('./EditPageContainer'));
 export default function PageRoutes({ path, history }) {
   return (
     <Switch path={path} history={history}>
+      <Route
+        exact
+        path="/pages"
+        render={({ location }) => (
+          <Redirect to={{ pathname: '/pages/about', state: { from: location } }} />
+        )}
+      />
       <Route exact path="/pages/new" component={NewPage} history={history} />
       <Switch>
         <Route path="/pages/:pageId/edit" component={EditPage} history={history} />
