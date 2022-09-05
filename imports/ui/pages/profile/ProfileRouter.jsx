9d545f7a9c94175dc16198e4ae0d23807d9f5e47
Emin Durak <emin@tuta.io>
2022-09-05 18:40:46 +0300

    updated profile ruter

--- imports/ui/pages/profile/ProfileRouter.jsx
+++ imports/ui/pages/profile/ProfileRouter.jsx
@@ -2,7 +2,7 @@ import React, { lazy } from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 
 const EditProfileContainer = lazy(() => import('./EditProfileContainer'));
-const MyWorks = lazy(() => import('../works/MyWorks'));
+const MemberWorks = lazy(() => import('../works/MemberWorks'));
 const Work = lazy(() => import('../works/Work'));
 const EditWork = lazy(() => import('../works/EditWork'));
 
@@ -13,11 +13,12 @@ export default function PageRoutes({ path, history }) {
   return (
     <Switch path={path} history={history}>
       <Route path="/@:username/edit" component={EditProfileContainer} />
-      <Route path="/@:username/works" component={MyWorks} />
+      {/* <Route path="/@:username/works" component={MemberWorks} /> */}
       <Route path="/@:username/works/:workId" component={Work} />
       <Route path="/@:username/works/:workId/edit" component={EditWork} />
       <Route path="/@:username/activities" component={MyActivities} />
-      <Route path="/@:username" component={MemberPublic} />
+      <Route path="/@:username" history={history} component={MemberPublic} />
+      <Route path="/@:username/:profileRoute" history={history} component={MemberPublic} />
     </Switch>
   );
 }
