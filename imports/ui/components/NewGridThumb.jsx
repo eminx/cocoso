80dcb524a04b0d8ca5c34b553956e6bc062fbdd8
Emin Durak <emin@tuta.io>
2023-01-22 01:53:40 +0300

    tag label indicating their host on each entry for listing pages

--- imports/ui/components/NewGridThumb.jsx
+++ imports/ui/components/NewGridThumb.jsx
@@ -1,52 +1,64 @@
-import React from 'react';
-import { Avatar, Box, Flex, Heading, Text } from '@chakra-ui/react';
+import React, { useContext } from 'react';
+import { Avatar, Box, Code, Flex, Heading, Tag as CTag, Text } from '@chakra-ui/react';
 import { LazyLoadImage } from 'react-lazy-load-image-component';
 
+import { StateContext } from '../LayoutContainer';
 import { DateJust } from './FancyDate';
 import Tag from './Tag';
 
 export default function GridThumb({ avatar, color, dates, imageUrl, subTitle, title, tag }) {
+  const { currentHost } = useContext(StateContext);
+
   if (!title || !imageUrl) {
     return null;
   }
 
   return (
-    <Box className="text-link-container" m="6" mb="40px">
-      <LazyLoadImage
-        alt={title}
-        effect="blur"
-        fit="contain"
-        src={imageUrl}
-        style={{
-          maxHeight: 300,
-          marginBottom: 12,
-        }}
-      />
-      <Flex align="flex-start" justify="space-between" mb="2">
-        <Box pr="2" maxW={300}>
-          <Heading className="text-link" fontSize="1.4rem" fontWeight="light" mb="2">
-            {title}
-          </Heading>
-          {subTitle && (
-            <Heading className="text-link" fontSize="md" fontWeight="light">
-              {subTitle}
+    <Box m="6" mb="40px">
+      <Box className="text-link-container">
+        <LazyLoadImage
+          alt={title}
+          effect="blur"
+          fit="contain"
+          src={imageUrl}
+          style={{
+            maxHeight: 300,
+            marginBottom: 6,
+          }}
+        />
+        <Flex align="flex-start" justify="space-between" mb="2">
+          <Box pr="2" maxW={300}>
+            <Heading className="text-link" fontSize="1.4rem" fontWeight="light" mb="1">
+              {title}
             </Heading>
-          )}
-          {tag && <Tag filterColor={color} label={tag} mb="1" />}
-        </Box>
-        {avatar && <Avatar name={avatar.name} src={avatar.url} />}
-        {dates && (
-          <Flex color="gray.800" fontWeight="light">
-            <DateJust style={{ color: '##2d2d2d' }}>{dates[0]}</DateJust>
-            {dates.length > 1 && (
-              <Text fontSize="xl" ml="2" wordBreak="keep-all">
-                {' '}
-                +{dates.length - 1}
-              </Text>
+            {subTitle && (
+              <Heading className="text-link" fontSize="md" fontWeight="light">
+                {subTitle}
+              </Heading>
             )}
-          </Flex>
-        )}
-      </Flex>
+            {tag && <Tag filterColor={color} label={tag} my="1" />}
+            {currentHost.isPortalHost && (
+              <Flex justify="flex-start">
+                <CTag size="md" my="2" border="1px solid #2d2d2d">
+                  {currentHost.host}
+                </CTag>
+              </Flex>
+            )}
+          </Box>
+          {avatar && <Avatar name={avatar.name} src={avatar.url} />}
+          {dates && (
+            <Flex color="gray.800" fontWeight="light">
+              <DateJust style={{ color: '##2d2d2d' }}>{dates[0]}</DateJust>
+              {dates.length > 1 && (
+                <Text fontSize="xl" ml="2" wordBreak="keep-all">
+                  {' '}
+                  +{dates.length - 1}
+                </Text>
+              )}
+            </Flex>
+          )}
+        </Flex>
+      </Box>
     </Box>
   );
 }
