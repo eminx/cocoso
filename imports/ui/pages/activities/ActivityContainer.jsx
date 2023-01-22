b3f8afbf6620938c9895dfac9c0af6f89dd60daf
Emin Durak <emin@tuta.io>
2023-01-23 02:07:42 +0300

    optimised to method from subscription

--- imports/ui/pages/activities/ActivityContainer.jsx
+++ imports/ui/pages/activities/ActivityContainer.jsx
@@ -1,39 +1,38 @@
-import React from 'react';
-import { withTracker } from 'meteor/react-meteor-data';
+import React, { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 
 import Activity from './Activity';
-// import Chats from '../../../api/chats/chat';
-import Activities from '../../../api/activities/activity';
-
-const ActivityContainer = withTracker((props) => {
-  const activityId = props.match.params.activityId;
-  const activity = Meteor.subscribe('activity', activityId);
-
-  const isLoading = !activity.ready();
-  const activityData = Activities ? Activities.findOne({ _id: activityId }) : null;
-  const currentUser = Meteor.user();
-
-  // const chatSubscription = Meteor.subscribe('chat', activityId);
-  // const chatData = Chats ? Chats.findOne({ contextId: activityId }) : null;
-
-  return {
-    isLoading,
-    activityData,
-    currentUser,
-    // chatData,
-  };
-})(Activity);
+import { call } from '../../utils/shared';
+import { message } from '../../components/message';
 
 export default function (props) {
+  const [activityData, setActivityData] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
   const [t] = useTranslation('activities');
   const [tc] = useTranslation('common');
 
+  useEffect(() => {
+    getActivityById();
+  }, []);
+
+  const { activityId } = props.match.params;
+  const getActivityById = async () => {
+    try {
+      setActivityData(await call('getActivityById', activityId));
+    } catch (error) {
+      message(error.reason);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   const allProps = {
     ...props,
+    activityData,
+    isLoading,
     t,
     tc,
   };
 
-  return <ActivityContainer {...allProps} />;
+  return <Activity {...allProps} />;
 }
