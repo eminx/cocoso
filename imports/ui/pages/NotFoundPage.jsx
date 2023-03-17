38c387183f89aa53a327bcfe493da201d928a8bf
Emin Durak <emin@tuta.io>
2023-03-18 01:18:37 +0300

    not found page with translations and link to home page

--- imports/ui/pages/NotFoundPage.jsx
+++ imports/ui/pages/NotFoundPage.jsx
@@ -1,24 +1,28 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Box, Link as CLink, Text } from '@chakra-ui/react';
+import { useTranslation } from 'react-i18next';
 import Template from '../components/Template';
 
-const NotFoundPage = ({ domain, history }) => (
-  <Template>
-    <Box m="4" p="2">
-      <Text textAlign="center" size="large" margin="2">
-        404
-      </Text>
-      <Text textAlign="center" fontWeight="bold">
-        {domain || 'Page'} could not be found
-      </Text>
-      <Text textAlign="center" mt="6">
-        <Link to="/">
-          <CLink color="#06c">Back to home page</CLink>
-        </Link>
-      </Text>
-    </Box>
-  </Template>
-);
+const NotFoundPage = ({ domain, history }) => {
+  const [t] = useTranslation('common');
+  return (
+    <Template>
+      <Box m="4" p="2">
+        <Text textAlign="center" size="large" margin="2">
+          404
+        </Text>
+        <Text textAlign="center" fontWeight="bold">
+          {t('labels.notfound.info')}
+        </Text>
+        <Text textAlign="center" mt="6">
+          <Link to="/">
+            <CLink color="#06c">{t('labels.notfound.gohome')}</CLink>
+          </Link>
+        </Text>
+      </Box>
+    </Template>
+  );
+};
 
 export default NotFoundPage;
