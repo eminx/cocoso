08645d0891b327d995f1b11d8a22252e8ce171fe
Emin Durak <emin@tuta.io>
2023-04-17 14:23:17 +0300

    grid thumb scaling better

--- imports/ui/components/NewGridThumb.jsx
+++ imports/ui/components/NewGridThumb.jsx
@@ -1,7 +1,7 @@
-import React, { useContext, useEffect, useState } from 'react';
-import { Avatar, Box, Flex, Heading, HStack, Text } from '@chakra-ui/react';
+import React, { useContext } from 'react';
+import { Avatar, Box, Center, Flex, Heading, HStack, Text } from '@chakra-ui/react';
 import { LazyLoadImage } from 'react-lazy-load-image-component';
-import { useImageSize } from 'react-image-size';
+// import { useImageSize } from 'react-image-size';
 
 import { StateContext } from '../LayoutContainer';
 import { DateJust } from './FancyDate';
@@ -10,10 +10,8 @@ import Tag from './Tag';
 const imageHeight = 280;
 
 export default function GridThumb({ avatar, color, dates, host, imageUrl, subTitle, title, tag }) {
-  const [dimensions, { loading, error }] = useImageSize(imageUrl);
+  // const [dimensions, { loading, error }] = useImageSize(imageUrl);
   const { currentHost } = useContext(StateContext);
-
-  useEffect(() => {}, []);
 
   if (!title || !imageUrl) {
     return null;
@@ -21,22 +19,23 @@ export default function GridThumb({ avatar, color, dates, host, imageUrl, subTit
 
   const remaining = dates?.length - 1;
 
-  const width = dimensions ? (dimensions.width * imageHeight) / dimensions.height : imageHeight;
+  // const width = dimensions ? (dimensions.width * imageHeight) / dimensions.height : imageHeight;
 
   return (
     <Box my="4" mx="5">
-      <Box className="text-link-container" position="relative" maxWidth={width}>
-        <LazyLoadImage
-          alt={title}
-          effect="blur"
-          fit="contain"
-          src={imageUrl}
-          style={{
-            position: 'relative',
-            maxHeight: imageHeight,
-            marginBottom: 6,
-          }}
-        />
+      <Box className="text-link-container" position="relative" maxWidth={420} minWidth={280}>
+        <Center mb="4" bg="gray.100">
+          <LazyLoadImage
+            alt={title}
+            effect="blur"
+            fit="contain"
+            src={imageUrl}
+            style={{
+              position: 'relative',
+              maxHeight: imageHeight,
+            }}
+          />
+        </Center>
         {host && currentHost.isPortalHost && (
           <Box position="absolute" top="0" right="0" pl="2" pb="2" bg="white">
             <Tag border="1px solid #2d2d2d" label={host} />
