91fc542ffd302b6da708b175151237a0052600b7
Emin Durak <emin@tuta.io>
2023-05-30 15:12:22 +0300

    mail translations sv

--- imports/api/processes/mailtranslations.js
+++ imports/api/processes/mailtranslations.js
@@ -34,35 +34,35 @@ export default {
   },
   sv: {
     general: {
-      dateAndTime: 'Date & Time',
-      dear: 'Dear',
+      dateAndTime: 'Datum & tid',
+      dear: 'Hej',
       person: 'person',
-      people: 'people',
-      title: 'Title',
-      processPage: 'Go to the page',
-      visitPage: 'Visit the page',
+      people: 'personer',
+      title: 'Titel',
+      processPage: 'Gå till sidan',
+      visitPage: 'Besök sidan',
     },
     joinProcess: {
-      confirmedApprovalText: 'You have successfully joined',
+      confirmedApprovalText: 'Du har nu gått med i gruppen/processen',
       confirmedApprovalTextLong:
-        'You are encouraged to follow the updates, register to attend meetings and join the discussion at the page',
-      confirmedApprovalTextLeave: 'You have successfully left',
-      confirmedApprovalTextLongLeave: 'If you want to join again, you can do so here at the page',
+        'Du kan nu följa uppdateringar, registrera dig för att delta i möten och vara i med i diskussioner på sidan.',
+      confirmedApprovalTextLeave: 'Du har nu lämnat gruppen/processen',
+      confirmedApprovalTextLongLeave: 'Om du vill gå med igen, kan du göra det här på sidan',
     },
     invitePrivateProcess: {
-      confirmedApprovalText: 'This is an email to invite you to a private process',
+      confirmedApprovalText: 'Detta mejl är en inbjudan till en privat grupp/process',
       confirmedApprovalTextLong:
-        'If you wish to accept this invite and join, simply go to the page and click the "Join" button. Please bear in mind that you have to have an account, or create one, with this email address to which you received this email. You are encouraged to follow the updates, register to attend meetings and join the discussion at this page.',
+        'Om du vill acceptera denna inbjudan och gå med i gruppen/processen, så går du till sidan och klicka ”Gå med”/”Join”. Notera att du måste ha ett konto, eller skapa ett med samma epostadress som du fick denna inbjudan till. Du är sedan välkommen att följa uppdateringar, registrera dig till kommande möten och delta i diskussionerna på gruppens/processens sida.',
     },
     attendMeeting: {
-      confirmedApprovalText: 'You have successfully registered your attendance for the meeting',
+      confirmedApprovalText: 'Du har registrerat dig till mötet',
       confirmedApprovalTextLong:
-        'You are encouraged to follow the updates, register to attend meetings and join the discussion at the page',
+        'Du är välkommen att följa uppdateringar, registrera dig till kommande möten och delta i diskussionerna på gruppens/processens sida.',
     },
     unattendMeeting: {
-      confirmedApprovalText: 'You have successfully removed your registration',
+      confirmedApprovalText: 'Du har tagit bort din registering till mötet',
       confirmedApprovalTextLong:
-        'You are encouraged to follow the updates, register to attend meetings and join the discussion at the page',
+        'Du är välkommen att följa uppdateringar, registrera dig till kommande möten och delta i diskussionerna på gruppens/processens sida.',
     },
   },
   tr: {
