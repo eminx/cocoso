0a87cfb9d1cbd269c064d5510315d953739491f1
Emin Durak <emin@tuta.io>
2023-06-03 16:42:50 +0300

    progress into ssr

--- imports/startup/client/index.jsx
+++ imports/startup/client/index.jsx
@@ -1,16 +1,12 @@
 import React, { Suspense } from 'react';
-import { Meteor } from 'meteor/meteor';
 import { onPageLoad } from 'meteor/server-render';
 import { hydrate } from 'react-dom';
 import { Progress } from '@chakra-ui/react';
-import '../i18n';
 
-import App from '../../ui/App';
+import '../i18n';
 import Routes from '../../ui/pages/Routes';
 
-// Meteor.startup(() => {
 onPageLoad(() => {
-  // hydrate(<App />, document.getElementById('render-target'));
   hydrate(
     <Suspense fallback={<Progress size="xs" colorScheme="pink" isIndeterminate />}>
       <Routes />
@@ -18,4 +14,3 @@ onPageLoad(() => {
     document.getElementById('render-target')
   );
 });
-// });
--- imports/startup/server/index.js
+++ imports/startup/server/index.js
@@ -2,7 +2,7 @@ import { Meteor } from 'meteor/meteor';
 import { onPageLoad } from 'meteor/server-render';
 import React from 'react';
 import { StaticRouter } from 'react-router';
-import { renderToNodeStream } from 'react-dom/server';
+import { renderToString } from 'react-dom/server';
 import { Accounts } from 'meteor/accounts-base';
 import { Helmet } from 'react-helmet';
 import { ServerStyleSheet } from 'styled-components';
@@ -12,21 +12,21 @@ import routes from '../../ui/pages/Routes';
 import './api';
 import './migrations';
 
-Meteor.startup(() => {
-  const smtp = Meteor.settings.mailCredentials.smtp;
+// Meteor.startup(() => {
+const smtp = Meteor.settings.mailCredentials.smtp;
 
-  process.env.MAIL_URL = `smtps://${encodeURIComponent(smtp.userName)}:${smtp.password}@${
-    smtp.host
-  }:${smtp.port}`;
-  Accounts.emailTemplates.resetPassword.from = () => smtp.fromEmail;
-  Accounts.emailTemplates.from = () => smtp.fromEmail;
-  Accounts.emailTemplates.resetPassword.text = function (user, url) {
-    const newUrl = url.replace('#/', '');
-    return `To reset your password, simply click the link below. ${newUrl}`;
-  };
-});
+process.env.MAIL_URL = `smtps://${encodeURIComponent(smtp.userName)}:${smtp.password}@${
+  smtp.host
+}:${smtp.port}`;
+Accounts.emailTemplates.resetPassword.from = () => smtp.fromEmail;
+Accounts.emailTemplates.from = () => smtp.fromEmail;
+Accounts.emailTemplates.resetPassword.text = function (user, url) {
+  const newUrl = url.replace('#/', '');
+  return `To reset your password, simply click the link below. ${newUrl}`;
+};
+// });
 
-export const render = async (sink) => {
+const render = async (sink) => {
   const context = {};
 
   const WrappedApp = (
@@ -35,18 +35,20 @@ export const render = async (sink) => {
     </StaticRouter>
   );
 
-  const sheet = new ServerStyleSheet();
   try {
-    const appJSX = sheet.collectStyles(WrappedApp);
-    const htmlStream = sheet.interleaveWithNodeStream(renderToNodeStream(appJSX));
-    sink.renderIntoElementById('render-target', htmlStream);
+    const sheet = new ServerStyleSheet();
 
-    // const html = renderToString(sheet.collectStyles(WrappedApp));
-    // const helmet = Helmet.renderStatic();
-    // sink.appendToHead(helmet.title.toString());
-    // sink.appendToHead(sheet.getStyleTags());
+    // const appJSX = sheet.collectStyles(WrappedApp);
+    // const htmlStream = sheet.interleaveWithNodeStream(renderToNodeStream(appJSX));
+    // sink.renderIntoElementById('render-target', htmlStream);
 
-    // sink.renderIntoElementById('render-target', html);
+    const html = renderToString(sheet.collectStyles(WrappedApp));
+    const helmet = Helmet.renderStatic();
+    sink.appendToHead(helmet.title.toString());
+    sink.appendToHead(helmet.meta.toString());
+    sink.appendToHead(sheet.getStyleTags());
+    // sink.appendToBody(WrappedApp);
+    sink.renderIntoElementById('render-target', html);
 
     // sink.appendToBody(`
     // <script>
