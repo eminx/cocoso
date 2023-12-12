313b46465d436b9383790d06729d673dc0ad371d
Emin Durak <emin@tuta.io>
2023-12-12 20:14:38 +0300

    page header reusable

---
+++ imports/ui/components/PageHeader.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import { Box, Flex, Divider, Text } from '@chakra-ui/react';
+
+import NewButton from './NewButton';
+import { Heading } from './Header';
+import { StateContext } from '../LayoutContainer';
+
+function PageHeader({ description, numberOfItems, showNewButton = true, children }) {
+  const { isDesktop } = useState(StateContext);
+
+  return (
+    <Box mb="12" mt="4" px="4" maxW="780px">
+      <Flex>
+        <Flex align="center" justify="flex-start">
+          <Box>
+            <Flex wrap="wrap">
+              <Heading numberOfItems={numberOfItems} />
+              {children}
+            </Flex>
+            <Box pt="2" pr="2">
+              <Divider borderColor="gray.500" maxW="690px" />
+              <Text fontSize={isDesktop ? 'lg' : 'md'} fontWeight="light" mt="1">
+                {description}
+              </Text>
+            </Box>
+          </Box>
+        </Flex>
+        {showNewButton && <NewButton />}
+      </Flex>
+    </Box>
+  );
+}
+
+export default PageHeader;
