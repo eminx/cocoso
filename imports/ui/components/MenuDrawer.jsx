3850ea93fb9edd58a9441d247807ffc871606818
Emin Durak <emin@tuta.io>
2024-01-27 22:53:13 +0300

    centered

--- imports/ui/components/MenuDrawer.jsx
+++ imports/ui/components/MenuDrawer.jsx
@@ -59,7 +59,7 @@ export default function MenuDrawer({ currentHost, isDesktop, platform }) {
           zIndex={1420}
         >
           {!isOpen ? (
-            <Box p="4">
+            <Box p="4" pt="6">
               <IconButton
                 _hover={{ bg: 'brand.500' }}
                 bg="gray.800"
