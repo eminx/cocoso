39a269264b1af82be25c93e343238d09f481b872
Emin Durak <emin@tuta.io>
2024-05-27 23:14:17 +0300

    email newsletter working on safari

--- imports/ui/pages/admin/EmailNewsletter/EmailPreview.jsx
+++ imports/ui/pages/admin/EmailNewsletter/EmailPreview.jsx
@@ -1,19 +1,18 @@
 import React from 'react';
-import {
-  Body,
-  Button,
-  Column,
-  Container,
-  Head,
-  Heading,
-  Hr,
-  Html,
-  Img,
-  Link,
-  Row,
-  Section,
-  Text,
-} from '@react-email/components';
+import { Body } from '@react-email/body';
+import { Button } from '@react-email/button';
+import { Column } from '@react-email/column';
+import { Container } from '@react-email/container';
+import { Head } from '@react-email/head';
+import { Heading } from '@react-email/heading';
+import { Hr } from '@react-email/hr';
+import { Html } from '@react-email/html';
+import { Img } from '@react-email/img';
+import { Link } from '@react-email/link';
+import { Row } from '@react-email/row';
+import { Section } from '@react-email/section';
+import { Text } from '@react-email/text';
+
 import renderHTML from 'react-render-html';
 import { useTranslation } from 'react-i18next';
 import moment from 'moment';
