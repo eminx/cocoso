0b9b10378608abb3eb0f299565502b03a127aa07
Emin Durak <emin@tuta.io>
2024-06-07 01:22:25 +0300

    cleanup

--- imports/startup/server/index.js
+++ imports/startup/server/index.js
@@ -1,19 +1,8 @@
 import { Meteor } from 'meteor/meteor';
-import React from 'react';
 import { renderWithSSR } from 'meteor/communitypackages:react-router-ssr';
+
 import { AppRoutesSSR } from '../../ssr/AppRoutes';
-
-// import { WebAppInternals } from 'meteor/webapp';
-
-// import { onPageLoad } from 'meteor/server-render';
-// import React from 'react';
-// import { StaticRouter } from 'react-router';
-// import { renderToPipeableStream } from 'react-dom/server';
 import { Accounts } from 'meteor/accounts-base';
-// import { Helmet } from 'react-helmet';
-// import { ServerStyleSheet } from 'styled-components';
-// import Routes from '../../ui/pages/Routes';
-// import Hosts from '../../api/hosts/host';
 
 import './api';
 import './migrations';
@@ -32,36 +21,6 @@ Meteor.startup(() => {
   };
 });
 
-// if (!Meteor.user()) {
 renderWithSSR(AppRoutesSSR, {
   renderTarget: 'root',
 });
-// }
-
-// const render = async (sink) => {
-//   const context = {};
-//   const WrappedApp = (
-//     <StaticRouter location={sink.request.url} context={context}>
-//       <Routes />
-//     </StaticRouter>
-//   );
-
-//   try {
-//     const sheet = new ServerStyleSheet();
-//     // const html = sheet.collectStyles(WrappedApp);
-//     // const htmlStream = sheet.interleaveWithNodeStream(renderToPipeableStream(html));
-//     const htmlStream = renderToPipeableStream(WrappedApp);
-//     // console.log(html);
-
-//     const helmet = Helmet.renderStatic();
-//     sink.appendToHead(helmet.title.toString());
-//     sink.appendToHead(helmet.meta.toString());
-//     sink.appendToHead(sheet.getStyleTags());
-
-//     sink.renderIntoElementById('root', htmlStream);
-//   } catch (e) {
-//     console.error(e);
-//   }
-// };
-
-// onPageLoad(render);
