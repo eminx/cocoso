b416086cc72da7affdafcffb8e799791e11b6a6c
Emin Durak <emin@tuta.io>
2024-08-08 16:00:11 +0300

    image slider with the dots and the arrows on desktop

--- imports/ui/components/NiceSlider.jsx
+++ imports/ui/components/NiceSlider.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Box, Center, Flex, Skeleton } from '@chakra-ui/react';
+import { Box, Center, Flex, HStack } from '@chakra-ui/react';
 import FsLightbox from 'fslightbox-react';
 import { Fade, Slide } from 'react-slideshow-image';
 import 'react-slideshow-image/dist/styles.css';
@@ -44,7 +44,23 @@ function NiceSlider({ alt, images, height = '400px', width = '100%', isFade = tr
   );
 }
 
+function Dots({ images, currentSlideIndex }) {
+  return (
+    <Center>
+      {images.length > 1 && (
+        <HStack p="2">
+          {images.map((image, index) =>
+            index === currentSlideIndex ? <FilledCircle key={image} /> : <EmptyCircle key={image} />
+          )}
+        </HStack>
+      )}
+    </Center>
+  );
+}
+
 function ImageHandler({ height, width, images, isFade, children }) {
+  const [currentSlideIndex, setCurrentSlideIndex] = useState(0);
+
   if (images?.length === 1) {
     return (
       <Flex h={height} justify={isFade ? 'flex-start' : 'center'}>
@@ -54,20 +70,64 @@ function ImageHandler({ height, width, images, isFade, children }) {
   } else if (isFade) {
     return (
       <Box className="slide-container" h={height} w={width}>
-        <Fade arrows={false} transitionDuration={400}>
+        <Fade
+          arrows
+          canSwipe
+          pauseOnHover={false}
+          transitionDuration={400}
+          onStartChange={(from, to) => setCurrentSlideIndex(to)}
+        >
           {images.map((image, index) => children(image, index))}
         </Fade>
+        <Dots currentSlideIndex={currentSlideIndex} images={images} />
       </Box>
     );
   } else {
     return (
       <Box className="slide-container" h={height} w={width}>
-        <Slide arrows={false} transitionDuration={400}>
+        <Slide
+          arrows={false}
+          transitionDuration={400}
+          onStartChange={(from, to) => setCurrentSlideIndex(to)}
+        >
           {images.map((image, index) => children(image, index))}
         </Slide>
+        <Dots currentSlideIndex={currentSlideIndex} images={images} />
       </Box>
     );
   }
+}
+
+function EmptyCircle() {
+  return (
+    <svg width="8" height="8" viewBox="-2 -2 8 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <circle
+        cx="2"
+        cy="2"
+        r="2.5"
+        transform="matrix(1 0 0 -1 1.10059 2.81995)"
+        fill="white"
+        stroke="black"
+        strokeWidth="1"
+      />
+    </svg>
+  );
+}
+
+function FilledCircle() {
+  return (
+    <svg width="8" height="8" viewBox="-2 -2 8 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <circle
+        cx="2"
+        cy="2"
+        r="2.5"
+        transform="matrix(1 0 0 -1 0.940552 2.81995)"
+        fill="#212121"
+        stroke="black"
+        strokeWidth="1"
+      />
+    </svg>
+  );
 }
 
 export default NiceSlider;
