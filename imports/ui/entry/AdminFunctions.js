f6b3568801a1e375ed487c676da63ff2aabe4e55
Emin Durak <emin@tuta.io>
2025-01-23 04:48:41 +0300

    Admin functions for activity

---
+++ imports/ui/entry/AdminFunctions.js
@@ -0,0 +1,70 @@
+import React, { useState } from 'react';
+import {
+  Box,
+  Center,
+  IconButton,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  Text,
+} from '@chakra-ui/react';
+import Settings from 'lucide-react/dist/esm/icons/settings';
+
+export default function AdminFunctions({ menuItems }) {
+  const [popup, setPopup] = useState('none');
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleClickItem = (item) => {
+    if (!item) {
+      return;
+    }
+    if (item.onClick && typeof item.onClick === 'function') {
+      item.onClick();
+      return;
+    }
+    setPopup(item.kind);
+    setTimeout(() => setIsOpen(false), 1000);
+  };
+
+  const onClose = () => {
+    setPopup('none');
+    setTimeout(() => setIsOpen(false), 1000);
+  };
+
+  return (
+    <>
+      <Center position="relative">
+        <Box pt="2" position="absolute" top="0" w="100px">
+          <Menu closeOnSelect closeOnBlur isOpen={isOpen} onClose={() => setIsOpen(false)}>
+            <MenuButton size="sm" onClick={() => setIsOpen(!isOpen)}>
+              <IconButton
+                _hover={{ bg: 'brand.100' }}
+                _active={{ bg: 'brand.200' }}
+                as="span"
+                bg="brand.50"
+                border="1px solid #fff"
+                borderRadius="50%"
+                icon={<Settings />}
+                variant="ghost"
+              />
+              <Text color="brand.400" fontSize="xs">
+                {/* {t('admin.admin')} */}
+                Admin
+              </Text>
+            </MenuButton>
+            <MenuList size="lg">
+              {menuItems().map((item) => (
+                <MenuItem key={item.kind} onClick={() => handleClickItem(item)}>
+                  {item.label}
+                </MenuItem>
+              ))}
+            </MenuList>
+          </Menu>
+        </Box>
+      </Center>
+
+      {menuItems(onClose).find((item) => item.kind === popup)?.component}
+    </>
+  );
+}
---
+++ imports/ui/pages/activities/components/ActivityAdminFunctions.js
@@ -0,0 +1,23 @@
+import React, { useContext } from 'react';
+import { useTranslation } from 'react-i18next';
+import { useNavigate } from 'react-router-dom';
+
+import AdminFunctions from '../../../entry/AdminFunctions';
+import { ActivityContext } from '../Activity';
+
+export default function ActivityAdminFunctions() {
+  const [tc] = useTranslation('common');
+  const navigate = useNavigate();
+  const { activity } = useContext(ActivityContext);
+
+  const menuItems = () => [
+    {
+      kind: 'document',
+      label: tc('actions.update'),
+      component: null,
+      onClick: () => navigate(`/activities/${activity?._id}/edit`),
+    },
+  ];
+
+  return <AdminFunctions menuItems={menuItems} />;
+}
