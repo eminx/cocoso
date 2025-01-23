19053425c3b5f0b43a7961e9d919c329d4278025
Emin Durak <emin@tuta.io>
2025-01-23 07:04:29 +0300

    deps cleanup

--- imports/startup/client/index.jsx
+++ imports/startup/client/index.jsx
@@ -1,16 +1,14 @@
 import { Meteor } from 'meteor/meteor';
 import React from 'react';
 import { createRoot, hydrateRoot } from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { onPageLoad } from 'meteor/server-render';
-import { Box, Center, ChakraProvider } from '@chakra-ui/react';
+import { ChakraProvider } from '@chakra-ui/react';
 
 import '../i18n';
 import AppRoutes from '../../ui/pages/Routes';
-import { Signup } from '/imports/ui/pages/auth';
-import Setup from '/imports/ui/pages/setup';
-import SetupHome from '/imports/ui/pages/setup';
-import { call } from '/imports/ui/utils/shared';
+import SetupHome from '../../ui/pages/setup';
+import { call } from '../../ui/utils/shared';
 
 onPageLoad(async () => {
   const container = document.getElementById('root');
