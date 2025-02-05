4108e8f336a07086113de91967853160af752f7a
Emin Durak <emin@tuta.io>
2025-02-05 13:23:45 +0300

    back to gray bg

--- imports/ui/layout/DummyWrapper.js
+++ imports/ui/layout/DummyWrapper.js
@@ -6,8 +6,8 @@ export default function DummyWrapper({ children }) {
   return (
     <HydrationProvider>
       <Box
-        bg="linear-gradient(0deg, var(--chakra-colors-purple-100) 0%, var(--chakra-colors-brand-100) 100%);"
-        // bg="gray.100"
+        // bg="linear-gradient(0deg, var(--chakra-colors-purple-100) 0%, var(--chakra-colors-brand-100) 100%);"
+        bg="gray.100"
         boxShadow="md"
         minH="110vh"
         mb="148px"
