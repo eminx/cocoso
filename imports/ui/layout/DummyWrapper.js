32acb485c188325787beee2b03eeb101670e3d43
Emin Durak <emin@tuta.io>
2025-03-21 02:42:30 +0300

    admin no top margin

--- imports/ui/layout/DummyWrapper.js
+++ imports/ui/layout/DummyWrapper.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import { Box } from '@chakra-ui/react';
 import { HydrationProvider } from 'react-hydration-provider';
+import { useLocation } from 'react-router-dom';
 
 export default function DummyWrapper({ children, animate = false, otherProps }) {
+  const location = useLocation();
+
+  const pathname = location?.pathname;
+
   let wrapperClass = 'wrapper';
-  if (animate) {
+  if (animate && !pathname.includes('admin')) {
     wrapperClass += ' mobile-wrapper';
   }
 
