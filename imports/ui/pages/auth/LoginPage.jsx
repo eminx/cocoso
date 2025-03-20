86ddf7f0ab01dc51ab2317bb30d37c41d7cec091
Emin Durak <emin@tuta.io>
2025-03-21 01:57:53 +0300

    auth revision with modal

--- imports/ui/pages/auth/LoginPage.jsx
+++ imports/ui/pages/auth/LoginPage.jsx
@@ -1,6 +1,6 @@
 import { Meteor } from 'meteor/meteor';
 import React, { useContext, useState } from 'react';
-import { Link, Navigate } from 'react-router-dom';
+import { Link, Navigate, useNavigate } from 'react-router-dom';
 import { Box, Center, Heading, Image, Link as CLink, Text } from '@chakra-ui/react';
 import { useTranslation } from 'react-i18next';
 
@@ -9,12 +9,14 @@ import ConfirmModal from '../../generic/ConfirmModal';
 import { Login } from './index';
 import { message } from '../../generic/message';
 import { call } from '../../utils/shared';
+import Modal from '../../generic/Modal';
 
 function LoginPage() {
   const [t] = useTranslation('accounts');
   const { currentUser, currentHost, platform, role } = useContext(StateContext);
   const [isSubmitted, setIsSubmitted] = useState(false);
   const [isJoinModal, setIsJoinModal] = useState(false);
+  const navigate = useNavigate();
 
   if (currentUser && ['participant', 'contributor', 'admin'].includes(role)) {
     return <Navigate to={`/@${currentUser.username}/profile`} />;
@@ -50,52 +52,59 @@ function LoginPage() {
       await call('setSelfAsParticipant');
       message.success(t('profile.message.participant'));
     } catch (error) {
-      console.error(error);
-      message.error(error.reason);
+      message.error(error.reason || error.error);
     }
   };
 
   return (
     <Box pb="8">
-      <Center>
-        <Box w="xs">
-          {platform && (
-            <Center p="4">
-              <Image w="240px" src={platform?.logo} />
+      <Modal
+        contentProps={{ h: 'auto' }}
+        isOpen
+        scrollBehavior="outside"
+        size="2xl"
+        onClose={() => navigate('/')}
+      >
+        <Center>
+          <Box w="xs">
+            {platform && (
+              <Center p="4">
+                <Image w="240px" src={platform?.logo} />
+              </Center>
+            )}
+
+            <Heading mb="4" size="md" textAlign="center">
+              {t('login.labels.title')}
+            </Heading>
+
+            <Center mb="6">
+              <Text>
+                {t('login.labels.subtitle')}{' '}
+                <Link to="/register">
+                  <CLink as="span" color="blue.500">
+                    <b>{t('actions.signup')}</b>
+                  </CLink>
+                </Link>
+              </Text>
             </Center>
-          )}
 
-          <Heading mb="4" size="md" textAlign="center">
-            {t('login.labels.title')}
-          </Heading>
-
-          <Center mb="6">
-            <Text>
-              {t('login.labels.subtitle')}{' '}
-              <Link to="/register">
-                <CLink as="span" color="blue.500">
-                  <b>{t('actions.signup')}</b>
-                </CLink>
-              </Link>
-            </Text>
-          </Center>
-
-          <Box bg="gray.50" borderColor="gray.300" borderWidth={1} mb="4" p="6">
-            <Login isSubmitted={isSubmitted} onSubmit={handleSubmit} />
+            <Box bg="gray.50" borderColor="gray.300" borderWidth={1} mb="4" p="6">
+              <Login isSubmitted={isSubmitted} onSubmit={handleSubmit} />
+            </Box>
+            <Center>
+              <Text textAlign="center">
+                {t('actions.forgot')}
+                <br />
+                <Link to="/forgot-password">
+                  <CLink as="span" color="blue.500">
+                    <b>{t('actions.reset')}</b>
+                  </CLink>
+                </Link>
+              </Text>
+            </Center>
           </Box>
-          <Center>
-            <Text textAlign="center">
-              {t('actions.forgot')}
-              <br />
-              <Link to="/forgot-password">
-                <CLink as="span" color="blue.500">
-                  <b>{t('actions.reset')}</b>
-                </CLink>
-              </Link>
-            </Text>
-          </Center>
-        </Box>
-      </Center>
+        </Center>
+      </Modal>
 
       <ConfirmModal
         title={t('profile.joinHost', { host: currentHost?.settings?.name })}
--- imports/ui/pages/auth/SignupPage.jsx
+++ imports/ui/pages/auth/SignupPage.jsx
@@ -1,16 +1,18 @@
 import React, { useContext } from 'react';
-import { Link, Navigate } from 'react-router-dom';
+import { Link, Navigate, useNavigate } from 'react-router-dom';
 import { Box, Center, Heading, Image, Link as CLink, Text } from '@chakra-ui/react';
 import { useTranslation } from 'react-i18next';
 
 import { StateContext } from '../../LayoutContainer';
 import { Signup } from './index';
 import { createAccount } from './functions';
+import Modal from '../../generic/Modal';
 // import NewPlatform from '';
 
 function SignupPage() {
   const [t] = useTranslation('accounts');
   const { currentUser, platform } = useContext(StateContext);
+  const navigate = useNavigate();
 
   if (currentUser && platform?.isFederationLayout) {
     return <Navigate to="/intro" />;
@@ -20,42 +22,50 @@ function SignupPage() {
 
   return (
     <Box pb="8">
-      <Center>
-        <Box w="sm">
-          {platform && (
-            <Center>
-              <Box>
-                <Center p="4">
-                  <Image w="240px" src={platform?.logo} />
-                </Center>
-                <Heading mb="4" size="md" textAlign="center">
-                  {t('signup.labels.title')}
-                </Heading>
-                {platform?.isFederationLayout && (
+      <Modal
+        contentProps={{ h: 'auto' }}
+        isOpen
+        scrollBehavior="outside"
+        size="2xl"
+        onClose={() => navigate('/')}
+      >
+        <Center>
+          <Box w="sm">
+            {platform && (
+              <Center>
+                <Box>
+                  <Center p="4">
+                    <Image w="240px" src={platform?.logo} />
+                  </Center>
+                  <Heading mb="4" size="md" textAlign="center">
+                    {t('signup.labels.title')}
+                  </Heading>
+                  {/* {platform?.isFederationLayout && ( */}
                   <Text textAlign="center">
                     {t('signup.labels.platform', { platform: platform?.name })}
                   </Text>
-                )}
-              </Box>
+                  {/* )} */}
+                </Box>
+              </Center>
+            )}
+
+            <Center pb="4">
+              <Text>
+                {t('signup.labels.subtitle')}{' '}
+                <Link to="/login">
+                  <CLink as="span" color="blue.500">
+                    <b>{t('actions.login')}</b>
+                  </CLink>
+                </Link>
+              </Text>
             </Center>
-          )}
 
-          <Center pb="4">
-            <Text>
-              {t('signup.labels.subtitle')}{' '}
-              <Link to="/login">
-                <CLink as="span" color="blue.500">
-                  <b>{t('actions.login')}</b>
-                </CLink>
-              </Link>
-            </Text>
-          </Center>
-
-          <Box bg="gray.50" borderColor="gray.300" borderWidth={1} mb="4" p="6">
-            <Signup onSubmit={(values) => createAccount(values)} />
+            <Box bg="gray.50" borderColor="gray.300" borderWidth={1} mb="4" p="6">
+              <Signup onSubmit={(values) => createAccount(values)} />
+            </Box>
           </Box>
-        </Box>
-      </Center>
+        </Center>
+      </Modal>
     </Box>
   );
 }
