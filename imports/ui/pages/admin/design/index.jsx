8e32fe560fc50ad923111e0265cf6f8dfd89d482
Emin Durak <emin@tuta.io>
2025-06-25 16:10:36 +0300

    starts up the design code

--- imports/ui/pages/admin/design/index.jsx
+++ imports/ui/pages/admin/design/index.jsx
@@ -8,9 +8,22 @@ import { Trans } from 'react-i18next';
 export default function DesignOptions() {
   const tabs = [
     {
-      title: <Trans i18nKey="admin:settings.tabs.color" />,
-      path: 'color',
-      content: <ColorPicker />,
+      // title: <Trans i18nKey="admin:settings.tabs.color" />,
+      title: 'General',
+      path: 'general',
+      content: <div>General</div>,
+    },
+    {
+      // title: <Trans i18nKey="admin:settings.tabs.color" />,
+      title: 'Header',
+      path: 'header',
+      content: <div>Header</div>,
+    },
+    {
+      // title: <Trans i18nKey="admin:settings.tabs.color" />,
+      title: 'Elements',
+      path: 'elements',
+      content: <div>Elements</div>,
     },
     {
       title: <Trans i18nKey="admin:settings.tabs.color" />,
