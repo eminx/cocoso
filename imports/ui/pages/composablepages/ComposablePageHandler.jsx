8e456600e8e573d1092351cb3cba677904faae31
Emin Durak <emin@tuta.io>
2025-11-22 16:05:56 +0300

    cp visible by admin when draft

--- imports/ui/pages/composablepages/ComposablePageHandler.jsx
+++ imports/ui/pages/composablepages/ComposablePageHandler.jsx
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useLoaderData } from 'react-router';
+import { useAtomValue } from 'jotai';
 
 import ComposablePageHybrid from '/imports/ui/entry/ComposablePageHybrid';
+import { roleAtom } from '/imports/state';
 
 export default function ComposablePageHandler({ Host, pageTitles }) {
   const { composablePage } = useLoaderData();
+  const role = useAtomValue(roleAtom);
 
-  if (!composablePage || !composablePage.isPublished) {
+  if (!composablePage || (!composablePage.isPublished && role !== 'admin')) {
     return null;
   }
 
