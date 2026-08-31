134fb9c85eaa78383054e2fde2b8100290ab92ea
Emin Durak <emin@tuta.io>
2026-08-31 19:42:20 +0100

    import ouath

--- imports/startup/server/index.js
+++ imports/startup/server/index.js
@@ -10,7 +10,7 @@ import { call } from '/imports/api/_utils/shared';
 import serverRenderer from './serverRenderer';
 import './api';
 import './migrations';
-// import './oauth';
+import './oauth';
 
 const { cdn_server } = Meteor.settings;
 
