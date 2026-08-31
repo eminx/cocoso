866dada8118f94984e02e571037ec32b88416a3f
Emin Durak <emin@tuta.io>
2026-08-31 19:26:06 +0100

    joinDate noted

--- imports/api/memberships/membership.js
+++ imports/api/memberships/membership.js
@@ -8,9 +8,12 @@ const Memberships = new Mongo.Collection('memberships');
 Memberships.schema = new SimpleSchema({
   userId: { type: String, regEx: SimpleSchema.RegEx.Id },
   host: Schemas.Hostname,
-  role: { type: String, allowedValues: ['participant', 'contributor', 'admin'] },
+  role: {
+    type: String,
+    allowedValues: ['participant', 'contributor', 'admin'],
+  },
   isPublic: { type: Boolean, defaultValue: true },
-  date: { type: Date },
+  joinDate: { type: Date },
 });
 
 Memberships.attachSchema(Memberships.schema);
@@ -18,7 +21,10 @@ Memberships.attachSchema(Memberships.schema);
 if (Meteor.isServer) {
   Meteor.startup(async () => {
     const raw = Memberships.rawCollection();
-    await raw.createIndex({ userId: 1, host: 1 }, { unique: true, name: 'userId_host_unique' });
+    await raw.createIndex(
+      { userId: 1, host: 1 },
+      { unique: true, name: 'userId_host_unique' }
+    );
     await raw.createIndex({ host: 1, role: 1 }, { name: 'host_role' });
   });
 }
