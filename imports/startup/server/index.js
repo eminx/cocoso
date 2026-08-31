19eb69ae37fdaabf8b8d33ad0ea0970ffa7179b8
Emin Durak <emin@tuta.io>
2026-08-31 19:22:20 +0100

    fix

--- imports/startup/server/index.js
+++ imports/startup/server/index.js
@@ -2,15 +2,15 @@ import { Meteor } from 'meteor/meteor';
 import { Accounts } from 'meteor/accounts-base';
 import { onPageLoad } from 'meteor/server-render';
 import React from 'react';
+
 import Hosts from '/imports/api/hosts/host';
 import Memberships from '/imports/api/memberships/membership';
-
 import { call } from '/imports/api/_utils/shared';
 
 import serverRenderer from './serverRenderer';
 import './api';
 import './migrations';
-import './oauth';
+// import './oauth';
 
 const { cdn_server } = Meteor.settings;
 
