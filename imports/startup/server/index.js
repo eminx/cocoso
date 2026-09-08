925fdfa1bc7039d4f0cdb3e0393bb5627f77839b
Emin Durak <emin@tuta.io>
2026-09-08 20:50:07 +0100

    better emails?

--- imports/startup/server/index.js
+++ imports/startup/server/index.js
@@ -20,8 +20,10 @@ function setupSMTP() {
   process.env.MAIL_URL = `smtps://${encodeURIComponent(smtp.userName)}:${
     smtp.password
   }@${smtp.host}:${smtp.port}`;
-  Accounts.emailTemplates.resetPassword.from = () => smtp.fromEmail;
-  Accounts.emailTemplates.from = () => smtp.fromEmail;
+  Accounts.emailTemplates.resetPassword.from = () =>
+    `${Meteor.settings.public.name} <${smtp.fromEmail}>`;
+  Accounts.emailTemplates.from = () =>
+    `${Meteor.settings.public.name} <${smtp.fromEmail}>`;
   Accounts.emailTemplates.resetPassword.text = function (user, url) {
     const newUrl = url.replace('#/', '');
     return `To reset your password, simply click the link below. ${newUrl}`;
